0fea17c2182eb10892b8cbf20549beeb9d5bbe44
jonsequitur <jonsequeira@gmail.com>
2017-03-01 23:04:41 -0800

    more build tinkering

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 REPOROOT="$DIR"
 export NUGET_PACKAGES=$REPOROOT/.nuget
 export NUGET_HTTP_CACHE_PATH=$REPOROOT/.nuget
-export DOTNET_INSTALL_DIR=.dotnet
+export DOTNET_INSTALL_DIR=$REPOROOT/.dotnet
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 mkdir $DOTNET_INSTALL_DIR
-curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash /dev/stdin --install-dir .dotnet
+curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash /dev/stdin --install-dir $DOTNET_INSTALL_DIR
+
 PATH="$DOTNET_INSTALL_DIR:$PATH"
 
-dotnet restore CommandLine-netcore.sln -r osx.10.11-x64 
+dotnet restore CommandLine-netcore.sln 
 dotnet publish ./dotnet/dotnet-netcore.csproj -r osx.10.11-x64 -f netcoreapp1.0
 chmod +x ./dotnet/bin/Debug/netcoreapp1.0/osx. 10.11-x64/publish/dotnet
