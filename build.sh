997468c2055577811f7f8a9b20bba252c5c47f76
jonsequitur <jonsequeira@gmail.com>
2017-03-01 22:40:25 -0800

    fix nuget folder paths

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-export NUGET_PACKAGES=.nuget
-export NUGET_HTTP_CACHE_PATH=.nuget
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+REPOROOT="$DIR"
+export NUGET_PACKAGES=$REPOROOT/.nuget
+export NUGET_HTTP_CACHE_PATH=$REPOROOT/.nuget
 export DOTNET_INSTALL_DIR=.dotnet
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 mkdir $DOTNET_INSTALL_DIR
