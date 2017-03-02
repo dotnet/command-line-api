00f1de97e8ce2b61781d9ac0f6485e94e3bcce27
jonsequitur <jonsequeira@gmail.com>
2017-03-01 23:18:36 -0800

    build diagnostics

--- build.sh
+++ build.sh
@@ -8,15 +8,19 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 REPOROOT="$DIR"
-export NUGET_PACKAGES="$REPOROOT/.nuget"
-export NUGET_HTTP_CACHE_PATH="$REPOROOT/.nuget"
+export NUGET_PACKAGES="$REPOROOT/.nuget/packages"
+#echo NUGET_PACKAGES=$NUGET_PACKAGES
+export NUGET_HTTP_CACHE_PATH="$REPOROOT/.nuget/packages"
+#echo NUGET_HTTP_CACHE_PATH=$NUGET_HTTP_CACHE_PATH
 export DOTNET_INSTALL_DIR="$REPOROOT/.dotnet"
+#echo DOTNET_INSTALL_DIR=$DOTNET_INSTALL_DIR
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
 mkdir $DOTNET_INSTALL_DIR
 curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash /dev/stdin --install-dir $DOTNET_INSTALL_DIR
 
 PATH="$DOTNET_INSTALL_DIR:$PATH"
 
-dotnet restore CommandLine-netcore.sln 
-dotnet publish ./dotnet/dotnet-netcore.csproj -r osx.10.11-x64 -f netcoreapp1.0
+dotnet restore CommandLine-netcore.sln -v:diag
+dotnet publish ./dotnet/dotnet-netcore.csproj -r osx.10.11-x64 -f netcoreapp1.0 -v:diag
 chmod +x ./dotnet/bin/Debug/netcoreapp1.0/osx. 10.11-x64/publish/dotnet
