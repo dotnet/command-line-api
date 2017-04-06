1783c0650ecf5e2523690cc26dcb71288e2ebeac
jonsequitur <jonsequeira@gmail.com>
2017-04-06 16:10:29 -0400

    allow de-prefixed option to have the same name as a sibling command

--- CommandLine/OptionSet.cs
+++ CommandLine/OptionSet.cs
@@ -6,8 +6,10 @@ namespace Microsoft.DotNet.Cli.CommandLine
 {
     public class OptionSet : OptionSet<Option>
     {
-        public override bool HasAlias(Option option, string alias) => option.HasAlias(alias);
+        public override bool HasAlias(Option option, string alias) =>
+            option.RawAliases.Contains(alias) ||
+            option.Aliases.Contains(alias);
 
-        protected override IReadOnlyCollection<string> AliasesFor(Option option) => option.Aliases;
+        protected override IReadOnlyCollection<string> AliasesFor(Option option) => option.RawAliases;
     }
 }
