0255130fcebf97f20e8176587841da71010535ba
Nate McMaster <nate.mcmaster@microsoft.com>
2018-05-18 20:18:35 -0700

    Start project dragonfruit

---
+++ samples/DragonFruit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DragonFruitSample
+{
+    class Program
+    {
+        /// <summary>
+        /// Hello
+        /// </summary>
+        /// <param name="flavor"></param>
+        /// <param name="count"></param>
+        static void Main(
+            string flavor,
+            int count = 1)
+        {
+            Console.WriteLine($"Creating {count} banana {(count == 1 ? "smoothie" : "smoothies")} with {flavor}");
+        }
+    }
+}
