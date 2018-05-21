2e93eba3eec5be0c7474f5e6edf3da3550980f06
Jon Sequeira <jonsequeira@gmail.com>
2018-05-21 13:12:52 -0700

    R: fix namespace

--- src/System.CommandLine/CommandDefinition.cs
+++ src/System.CommandLine/CommandDefinition.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
 using System.Linq;
 
 namespace System.CommandLine
--- src/System.CommandLine/Invocation/MethodBinder.cs
+++ src/System.CommandLine/Invocation/MethodBinder.cs
@@ -3,7 +3,7 @@
 
 using System.Collections.Generic;
 
-namespace System.CommandLine
+namespace System.CommandLine.Invocation
 {
     public class MethodBinder
     {
