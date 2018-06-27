b670f4c3413c4a525d540373f54c4f99a824f9bb
Kevin B <kitokeboo@gmail.com>
2018-06-27 13:45:04 -0700

    Added ability to specify option argument and assume expected option.
    Changed the name of the first parameter of Parse to be more external user-friendly 'arguments' rather than 'rawTokens'. But keep rawTokens internally for consistency within the parser.
    
    Need to resolve how to pick 'last' command
    
    Added ability to specify option argument and assume expected option.
    
    Changed the name of the first parameter of Parse to be more external user-friendly 'arguments' rather than 'rawTokens'. But keep rawTokens internally for consistency within the parser.
    
    Need to resolve how to pick 'last' command
    
    Added ability to specify option argument and assume expected option.
    
    Changed the name of the first parameter of Parse to be more external user-friendly 'arguments' rather than 'rawTokens'. But keep rawTokens internally for consistency within the parser.
    
    Debugging after rebase
    
    Prepare for rebase
    
    Rebased to master - still need to fix tests and rework options to belong to the previous or root command.
    
    Fixed parser for all tests prior to 'options per command' change.
    
    Compile fixes after rebase.
    
    Lots of fixes around implicit options.
    
    A little refactoring to make it easier to read
    
    removing AddCommandFromMethodTests
    
    GatherOptions now returns IList<OptionDefinition>
    
    Adding additional unit tests to cover various subcommand cases.

--- src/System.CommandLine.DragonFruit/CommandLine.cs
+++ src/System.CommandLine.DragonFruit/CommandLine.cs
@@ -4,7 +4,6 @@
 using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
--- src/System.CommandLine.Tests/OptionTests.cs
+++ src/System.CommandLine.Tests/OptionTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.CommandLine.Builder;
-using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -138,6 +137,80 @@ namespace System.CommandLine.Tests
                   .Should()
                   .BeEquivalentTo("-h", "--help", "/?");
         }
+
+        [Fact]
+        public void When_option_argument_is_provided_without_option_name_argument_position_is_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_without_option_name_argument_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_first_option_name_argument_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-a value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("-")]
+        [InlineData("--")]
+        [InlineData("/")]
+        public void When_option_use_differnt_prefixes_they_still_work(string prefix)
+        {
+            var result = new CommandLineBuilder()
+                .AddOption(prefix + "a", "", a => a.ExactlyOne())
+                .AddOption(prefix + "b", "")
+                .AddOption(prefix + "c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse(prefix + "c value-for-c " + prefix + "a value-for-a");
+
+            result.ValueForOption(prefix + "a").Should().Be("value-for-a");
+            result.ValueForOption(prefix + "c").Should().Be("value-for-c");
+            result.HasOption(prefix + "b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_second_option_is_first_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                .AddOption("-a", "", a => a.ExactlyOne())
+                .AddOption("-b", "")
+                .AddOption("-c", "", a => a.ExactlyOne())
+                .Build()
+                .Parse("-c value-for-c value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
         
         [Theory]
         [InlineData(":", "-x{0}")]
@@ -179,5 +252,5 @@ namespace System.CommandLine.Tests
             result.ValueForOption(prefix + "c").Should().Be("value-for-c");
             result.HasOption(prefix + "b").Should().BeFalse();
         }
-   }
+    }
 }
