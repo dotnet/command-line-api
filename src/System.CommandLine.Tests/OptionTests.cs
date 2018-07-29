d52d0684522b867b8a0bf7e4bf1587a156eef582
Jingyu Ma <jingm@microsoft.com>
2018-07-29 13:21:20 +0800

    Added a test for calling WithHelp multi-times.

--- src/System.CommandLine.Tests/OptionTests.cs
+++ src/System.CommandLine.Tests/OptionTests.cs
@@ -291,5 +291,24 @@ namespace System.CommandLine.Tests
             option.Help.Description.Should().Be("helpDesc");
             option.Help.IsHidden.Should().BeTrue();
         }
+
+        [Fact]
+        public void When_option_provide_help_from_WithHelp_multiple_times_the_exposed_help_is_correct()
+        {
+            var optionBuilder = new OptionBuilder(
+                new[] { "-o", "--option" },
+                new CommandBuilder("optionCommand"));
+
+            var option1 = optionBuilder.WithHelp(new HelpDetail("helpName1", "helpDesc1", true)).BuildOption();
+            var option2 = optionBuilder.WithHelp(new HelpDetail("helpName2", "helpDesc2", false)).BuildOption();
+
+            option1.Help.Name.Should().Be("helpName1");
+            option1.Help.Description.Should().Be("helpDesc1");
+            option1.Help.IsHidden.Should().BeTrue();
+
+            option2.Help.Name.Should().Be("helpName2");
+            option2.Help.Description.Should().Be("helpDesc2");
+            option2.Help.IsHidden.Should().BeFalse();
+        }
     }
 }
