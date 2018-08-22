5b49c5bb985676245fdea64513993fc7e5738079
William Li <wul@microsoft.com>
2018-08-21 21:05:48 -0700

    Rename command name to dotnet-suggest

--- src/System.CommandLine.CompletionSuggestions/Register-ArgumentCompleter.ps1
+++ src/System.CommandLine.CompletionSuggestions/Register-ArgumentCompleter.ps1
@@ -1,10 +1,10 @@
-$availableToComplete = (completion-suggestions list) | Out-String
+$availableToComplete = (dotnet-suggest list) | Out-String
 $availableToCompleteArray = $availableToComplete.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries) |  ForEach-Object {$_.Trim() }
 
 Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
     param($commandName, $wordToComplete, $cursorPosition)
     $fullpath = (Get-Command $wordToComplete.CommandElements[0]).Source
-    $result = (completion-suggestions -p $cursorPosition -e $fullpath $wordToComplete.CommandElements) | Out-String
+    $result = (dotnet-suggest -p $cursorPosition -e $fullpath $wordToComplete.CommandElements) | Out-String
     $result.Trim() | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
