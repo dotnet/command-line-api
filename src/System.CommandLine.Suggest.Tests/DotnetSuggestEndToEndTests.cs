34f5ac24bfbd657f12cb19fbb71597098f4ccad0
Jon Sequeira <jonsequeira@gmail.com>
2018-09-22 12:10:06 -0700

    write diagnostic output to debug test

--- src/System.CommandLine.Suggest.Tests/DotnetSuggestEndToEndTests.cs
+++ src/System.CommandLine.Suggest.Tests/DotnetSuggestEndToEndTests.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
+using static System.Environment;
 
 namespace System.CommandLine.Suggest.Tests
 {
@@ -64,19 +65,18 @@ namespace System.CommandLine.Suggest.Tests
 
             stdOut.ToString()
                   .Should()
-                  .Be($"--apple{Environment.NewLine}--banana{Environment.NewLine}--durian{Environment.NewLine}");
+                  .Be($"--apple{NewLine}--banana{NewLine}--durian{NewLine}");
         }
 
         [ReleaseBuildOnlyFact]
         public async Task dotnet_suggest_provides_completions_for_app()
         {
             // run once to trigger a call to dotnet-suggest register
-            await Process.ExecuteAsync
-            (_endToEndTestApp.FullName,
-             "-h",
-             stdOut: s => _output.WriteLine(s),
-             stdErr: s => _output.WriteLine(s)
-            );
+            await Process.ExecuteAsync(
+                _endToEndTestApp.FullName,
+                "-h",
+                stdOut: s => _output.WriteLine(s),
+                stdErr: s => _output.WriteLine(s));
 
             var stdOut = new StringBuilder();
             var stdErr = new StringBuilder();
@@ -88,13 +88,16 @@ namespace System.CommandLine.Suggest.Tests
                 stdErr: value => stdErr.AppendLine(value),
                 environmentVariables: _environmentVariables);
 
+            _output.WriteLine($"stdOut:{NewLine}{stdOut}{NewLine}");
+            _output.WriteLine($"stdErr:{NewLine}{stdErr}{NewLine}");
+
             stdErr.ToString()
                   .Should()
                   .BeEmpty();
 
             stdOut.ToString()
                   .Should()
-                  .Be($"--apple{Environment.NewLine}--banana{Environment.NewLine}--durian{Environment.NewLine}");
+                  .Be($"--apple{NewLine}--banana{NewLine}--durian{NewLine}");
         }
     }
 }
