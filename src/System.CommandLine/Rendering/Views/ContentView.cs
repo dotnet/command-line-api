e6b78252aa7bff04c85cc14aa9554b96963912a4
Kevin B <kitokeboo@gmail.com>
2018-10-01 00:15:19 -0700

    Separating ContnetView<T> into its own file.

--- src/System.CommandLine/Rendering/Views/ContentView.cs
+++ src/System.CommandLine/Rendering/Views/ContentView.cs
@@ -4,36 +4,6 @@ using System.Reflection;
 
 namespace System.CommandLine.Rendering.Views
 {
-    public class ContentView<T> : ContentView
-    {
-        public ContentView(T value)
-        {
-            Value = value;
-        }
-
-        public T Value { get; }
-
-        public override Size Measure(IRenderer renderer, Size maxSize)
-        {
-            EnsureSpanCreated(renderer);
-            return base.Measure(renderer, maxSize);
-        }
-
-        public override void Render(IRenderer renderer, Region region)
-        {
-            EnsureSpanCreated(renderer);
-            base.Render(renderer, region);
-        }
-
-        private void EnsureSpanCreated(IRenderer renderer)
-        {
-            if (Span == null)
-            {
-                Span = renderer.Formatter.Format(Value);
-            }
-        }
-    }
-
     public class ContentView : View
     {
         public ContentView(string content)
