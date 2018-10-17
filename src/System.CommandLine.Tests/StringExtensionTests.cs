25068997037c354ce5eff960c9b50496aaa91e82
Jon Sequeira <jonsequeira@gmail.com>
2018-10-17 09:09:03 -0700

    R: move StringExtensionsTests to System.CommandLine.Tests

--- src/System.CommandLine.Tests/StringExtensionTests.cs
+++ src/System.CommandLine.Tests/StringExtensionTests.cs
@@ -1,11 +1,10 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using FluentAssertions;
-using System.Linq;
 using Xunit;
 
-namespace System.CommandLine.DragonFruit.Tests
+namespace System.CommandLine.Tests
 {
     public class StringExtensionTests
     {
