f609a9487c2ff489d61614de15fae7bbc569a9f0
Jon Sequeira <jonsequeira@gmail.com>
2018-10-31 15:46:19 -0700

    reorganize some tests for positional options and builders

--- src/System.CommandLine.Tests/PositionalOptionTests.cs
+++ src/System.CommandLine.Tests/PositionalOptionTests.cs
@@ -12,7 +12,8 @@ namespace System.CommandLine.Tests
         public void When_an_option_has_a_default_value_then_a_given_positional_value_should_overriden()
         {
             var configuration = new CommandLineConfiguration(
-                new[] {
+                new[]
+                {
                     new Option(
                         "-x",
                         "",
@@ -39,7 +40,8 @@ namespace System.CommandLine.Tests
         public void When_an_option_has_a_default_value_then_a_given_positional_value_should_override_with_other_specified()
         {
             var configuration = new CommandLineConfiguration(
-                new[] {
+                new[]
+                {
                     new Option(
                         "-x",
                         "",
@@ -92,13 +94,15 @@ namespace System.CommandLine.Tests
         {
             var parser = new CommandLineBuilder()
                          .EnablePositionalOptions()
-                         .AddCommand("subcommand1", symbols: b => {
+                         .AddCommand("subcommand1", symbols: b =>
+                         {
                              foreach (int optionIndex in Enumerable.Range(1, subcommand1Options))
                              {
                                  b.AddOption($"-anon{optionIndex}", arguments: argumentsBuilder => argumentsBuilder.ExactlyOne());
                              }
 
-                             b.AddCommand("subcommand2", symbols: subCommandBuilder => {
+                             b.AddCommand("subcommand2", symbols: subCommandBuilder =>
+                             {
                                  foreach (int optionIndex in Enumerable.Range(1, subcommand2Options))
                                  {
                                      subCommandBuilder.AddOption($"-anon{optionIndex}",
@@ -154,6 +158,66 @@ namespace System.CommandLine.Tests
             result.Errors.Should().BeEmpty();
             result.CommandResult["-anon1"].GetValueOrDefault<string>().Should().Be("anon1-value");
             result.CommandResult["-anon2"].GetValueOrDefault<string>().Should().Be("anon2-value");
+        }
+
+        [Fact]
+        public void When_option_argument_is_provided_without_option_name_then_argument_position_is_assumed()
+        {
+            var result = new CommandLineBuilder()
+                         .EnablePositionalOptions()
+                         .AddOption("-a", "", a => a.ExactlyOne())
+                         .Build()
+                         .Parse("value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_without_option_name_then_argument_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                         .EnablePositionalOptions()
+                         .AddOption("-a", "", a => a.ExactlyOne())
+                         .AddOption("-b", "")
+                         .AddOption("-c", "", a => a.ExactlyOne())
+                         .Build()
+                         .Parse("value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_first_option_name_then_argument_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                         .EnablePositionalOptions()
+                         .AddOption("-a", "", a => a.ExactlyOne())
+                         .AddOption("-b", "")
+                         .AddOption("-c", "", a => a.ExactlyOne())
+                         .Build()
+                         .Parse("-a value-for-a value-for-c");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
+        }
+
+        [Fact]
+        public void When_multiple_option_arguments_are_provided_with_second_option_is_first_positions_are_assumed()
+        {
+            var result = new CommandLineBuilder()
+                         .EnablePositionalOptions()
+                         .AddOption("-a", "", a => a.ExactlyOne())
+                         .AddOption("-b", "")
+                         .AddOption("-c", "", a => a.ExactlyOne())
+                         .Build()
+                         .Parse("-c value-for-c value-for-a");
+
+            result.ValueForOption("-a").Should().Be("value-for-a");
+            result.ValueForOption("-c").Should().Be("value-for-c");
+            result.HasOption("-b").Should().BeFalse();
         }
     }
 }
