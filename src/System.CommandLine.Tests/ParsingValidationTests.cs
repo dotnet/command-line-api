43c30e2c8bcbe1458541cf3f216aa179a5cb01dc
Jon Sequeira <jonsequeira@gmail.com>
2018-12-12 17:41:15 -0800

    allow invocation of intermediate commands

--- src/System.CommandLine.Tests/ParsingValidationTests.cs
+++ src/System.CommandLine.Tests/ParsingValidationTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
 using System.IO;
 using System.Linq;
 using FluentAssertions;
@@ -224,7 +225,7 @@ namespace System.CommandLine.Tests
         }
 
         [Fact]
-        public void When_there_are_subcommands_and_options_then_a_subcommand_must_be_provided()
+        public void A_command_with_subcommands_is_invalid_to_invoke_if_it_has_no_handler()
         {
             var outer = new Command("outer");
             var inner = new Command("inner");
@@ -239,6 +240,26 @@ namespace System.CommandLine.Tests
                   .ContainSingle(
                       e => e.Message.Equals(ValidationMessages.Instance.RequiredCommandWasNotProvided()) &&
                            e.SymbolResult.Name.Equals("inner"));
+        }
+
+        [Fact]
+        public void A_command_with_subcommands_is_valid_to_invoke_if_it_has_a_handler()
+        {
+            var outer = new Command("outer");
+            var inner = new Command("inner")
+                        {
+                            Handler = CommandHandler.Create(() =>
+                            {
+                            })
+                        };
+            var innerer = new Command("inner-er");
+            outer.AddCommand(inner);
+            inner.AddCommand(innerer);
+
+            var result = outer.Parse("outer inner");
+
+            result.Errors.Should().BeEmpty();
+            result.CommandResult.Command.Should().Be(inner);
         }
 
         [Fact]
