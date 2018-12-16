e8de61ee82274f2537ba14e897517299a7116d3c
William Li <wul@microsoft.com>
2018-12-16 15:27:04 -0800

    No need extra restore during test
    And it also removed the race for generating it

--- src/System.CommandLine.Suggest.Tests/get-completions.sh
+++ src/System.CommandLine.Suggest.Tests/get-completions.sh
@@ -17,13 +17,9 @@ get_completion_function_name_from_complete_output()
 get_completions()
 {
     COMP_WORDS=("$@") # completion to test
-    #echo "COMP_WORDS: '${COMP_WORDS[@]}'"
     COMP_LINE="$(local IFS=" "; echo "${COMP_WORDS[@]}")"
-    #echo "COMP_LINE: '$COMP_LINE'"
     COMP_CWORD=$((${#COMP_WORDS[@]} - 1)) # index into COMP_WORDS
-    #echo "COMP_CWORD: '$COMP_CWORD'"
     COMP_POINT="${#COMP_LINE}" # index of cursor position
-    #echo "COMP_POINT: '$COMP_POINT'"
 
     source dotnet-suggest-shim.bash
 
