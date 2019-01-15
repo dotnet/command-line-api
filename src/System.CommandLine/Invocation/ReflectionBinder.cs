9dcacd353880999e16b3dff3cbbae0ce371ba76f
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2019-01-15 12:18:59 -0600

    WIP: Working on passing tests

---
+++ src/System.CommandLine/Invocation/ReflectionBinder.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace System.CommandLine.Invocation
+{
+    public class ReflectionBinder
+    {
+        protected const BindingFlags CommonBindingFlags = BindingFlags.FlattenHierarchy
+                                    | BindingFlags.IgnoreCase
+                                    | BindingFlags.Public
+                                    | BindingFlags.NonPublic;
+
+    }
+
+    public class ReflectionBinder<TTarget> : ReflectionBinder
+        where TTarget : class
+    {
+        public BindingSet BindingSet { get; } = new BindingSet();
+        public Func<InvocationContext, TTarget> CreateTargetFunc { get; set; }
+        public TTarget Target { get; set; }
+
+        public void AddBinding(BindingBase bindingAction)
+            => BindingSet.AddBinding(bindingAction);
+
+        public void AddBindings(IEnumerable<BindingBase> bindingActions)
+            => BindingSet.AddBindings(bindingActions);
+
+        public BindingBase Find(object reflectionObject)
+            => BindingSet.Find(reflectionObject);
+
+        public TTarget CreateInstance(InvocationContext context)
+        {
+            SetStaticProperties(context);
+            var target = Target
+                         ?? (CreateTargetFunc == null
+                            ? CreateTarget(context)
+                            : CreateTargetFunc?.Invoke(context));
+
+            SetTargetProperties(context,target);
+            return target;
+        }
+
+        private void SetTargetProperties(InvocationContext context, TTarget target)
+        {
+            var type = typeof(TTarget);
+            var properties = type.GetProperties(CommonBindingFlags | BindingFlags.Instance);
+            SetProperties(context, target, properties);
+        }
+
+        private TTarget CreateTarget(InvocationContext context)
+        {
+            var type = typeof(TTarget);
+            // TODO: Figure out if we should restrict to public constructors
+            var ctors = type.GetConstructors();
+            if (ctors.Count() > 1)
+            {
+                throw new InvalidOperationException("Target cannot have multiple constructors");
+            }
+            var ctor = ctors.FirstOrDefault();
+            return ctor.GetParameters().Count() == 0
+                    ? Activator.CreateInstance<TTarget>()
+                    : GetTargetFromConstructor(context, ctor);
+        }
+
+        private TTarget GetTargetFromConstructor(InvocationContext context, ConstructorInfo constructorInfo)
+        {
+            var type = typeof(TTarget);
+            var paramValues = GetParameterValues(context, null, constructorInfo);
+            return (TTarget)Activator.CreateInstance(type, paramValues);
+        }
+
+        public object[] GetParameterValues(InvocationContext context, TTarget target, MethodBase methodInfo)
+        {
+            var parameters = methodInfo.GetParameters();
+            var values = new List<object>();
+            foreach (var param in parameters)
+            {
+                var binding = BindingSet.Find(param);
+                values.Add(GetParameterValue(context, target, param, binding));
+            }
+            return values.ToArray();
+        }
+
+        private object GetParameterValue(InvocationContext context,TTarget target,  ParameterInfo param, BindingBase binding)
+        {
+            if (binding != null)
+            {
+                return ValueFromBinding(context, target, binding);
+            }
+            var value = context.ServiceProvider.GetService(param.ParameterType);
+            return value ?? (param.HasDefaultValue
+                                ? param.DefaultValue
+                                : param.ParameterType.GetDefaultValue());
+        }
+
+        private object ValueFromBinding(InvocationContext context, TTarget target, BindingBase binding)
+        {
+            object value;
+            switch (binding)
+            {
+                case FuncBinding<TTarget> funcBinding:
+                    value = funcBinding.ValueFunc(context, target);
+                    break;
+                case SymbolBinding symbolBinding:
+                    switch (symbolBinding.Symbol)
+                    {
+                        case Option option:
+                            value = context.ParseResult.GetValue(option);
+                            break;
+                        case Argument argument:
+                            value = context.ParseResult.GetValue(argument);
+                            break;
+                        default:
+                            throw new InvalidOperationException("Internal: Unknown symbol type encountered");
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException("Internal: Symbol or value func missing in BindAction");
+            }
+            // TODO: Handle conversion errors - needs to propagate so needs thought
+            var typedValue = Convert.ChangeType(value, binding.ReturnType);
+            return typedValue;
+        }
+
+        private void SetStaticProperties(InvocationContext context)
+        {
+            var bindingFlags = CommonBindingFlags | BindingFlags.Static;
+            var type = typeof(TTarget);
+            var staticProperties = type.GetProperties(bindingFlags);
+            SetProperties(context, null, staticProperties);
+            if (type.IsNested)
+            {
+                // TODO: How do you find a nested types parent?
+                // TODO: Set nested parents static properties
+            }
+        }
+
+        private void SetProperties(InvocationContext context, object target, IEnumerable<PropertyInfo> properties)
+        {
+            foreach (var prop in properties)
+            {
+                var binding = BindingSet.Find(prop);
+                object value = binding == null
+                                ? context.ServiceProvider.GetService(prop.PropertyType)
+                                : ValueFromBinding(context, null, binding);
+                prop.SetValue(target, value);
+            }
+        }
+
+    }
+}
--- src/System.CommandLine/Invocation/ReflectionCommandHandler.cs
+++ src/System.CommandLine/Invocation/ReflectionCommandHandler.cs
@@ -23,7 +23,6 @@ namespace System.CommandLine.Invocation
 
     public abstract class ReflectionCommandHandler : ICommandHandler
     {
-        public BindingSet BindingSet { get; } = new BindingSet();
 
         public static ReflectionCommandHandler Create(MethodInfo methodInfo)
         {
@@ -58,7 +57,7 @@ namespace System.CommandLine.Invocation
                 where TTarget : class
         {
             var handler = Activator.CreateInstance<ReflectionCommandHandler<TTarget>>();
-            handler.Target = target;
+            handler.Binder.Target = target;
             methodInfo = methodInfo ?? GetInvokeMethod(typeof(TTarget));
             handler.InvocationMethodInfo = methodInfo;
             return handler;
@@ -72,12 +71,6 @@ namespace System.CommandLine.Invocation
                                      .FirstOrDefault();
         }
 
-        public void AddBinding(BindingBase bindingAction)
-            => BindingSet.AddBinding(bindingAction);
-
-        public void AddBindings(IEnumerable<BindingBase> bindingActions)
-            => BindingSet.AddBindings(bindingActions);
-
         public abstract Task<int> InvokeAsync(InvocationContext context);
 
         protected MethodInfo InvocationMethodInfo { get; set; }
@@ -86,154 +79,20 @@ namespace System.CommandLine.Invocation
     public class ReflectionCommandHandler<TTarget> : ReflectionCommandHandler
         where TTarget : class
     {
-        private const BindingFlags CommonBindingFlags = BindingFlags.FlattenHierarchy
-                                            | BindingFlags.IgnoreCase
-                                            | BindingFlags.Public
-                                            | BindingFlags.NonPublic;
-        public TTarget Target { get; set; }
-        private InvocationContext context;
-        public Func<InvocationContext, TTarget> CreateTargetFunc { get; set; }
+        public ReflectionBinder<TTarget> Binder { get; set; } = new ReflectionBinder<TTarget>();
 
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         // TODO: How do we await this via reflection?
         public override Task<int> InvokeAsync(InvocationContext context)
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
-            this.context = context;
-            SetStaticProperties();
-
-            var target = Target
-                         ?? (CreateTargetFunc == null
-                            ? CreateTarget()
-                            : CreateTargetFunc?.Invoke(context));
-
-            SetTargetProperties(target);
-
-            var methodArguments = GetParameterValues(target, InvocationMethodInfo);
+            var target = Binder.CreateInstance(context);
+            var methodArguments = Binder.GetParameterValues(context,target, InvocationMethodInfo);
 
             var value = InvocationMethodInfo.Invoke(target, methodArguments);
 
-            // release reference to context, since it's only valid during invoke
-            this.context = null;
-
             return CommandHandler.GetResultCodeAsync(value, context);
-
-            //switch (ret)
-            //{
-            //    case int i:
-            //        return i;
-            //    case Task<int> task:
-            //        return await task;
-            //    default:
-            //        return default(int);
-            //}
         }
 
-        private void SetTargetProperties(TTarget target)
-        {
-            var type = typeof(TTarget);
-            var properties = type.GetProperties(CommonBindingFlags | BindingFlags.Instance);
-            SetProperties(target, properties);
-        }
-
-        private TTarget CreateTarget()
-        {
-            var type = typeof(TTarget);
-            // TODO: Figure out if we should restrict to public constructors
-            var ctors = type.GetConstructors();
-            if (ctors.Count() > 1)
-            {
-                throw new InvalidOperationException("Target cannot have multiple constructors");
-            }
-            var ctor = ctors.FirstOrDefault();
-            return ctor.GetParameters().Count() == 0
-                    ? Activator.CreateInstance<TTarget>()
-                    : GetTargetFromConstructor(ctor);
-        }
-
-        private TTarget GetTargetFromConstructor(ConstructorInfo constructorInfo)
-        {
-            var type = typeof(TTarget);
-            var paramValues = GetParameterValues(null, constructorInfo);
-            return (TTarget)Activator.CreateInstance(type, paramValues);
-        }
-
-        private object[] GetParameterValues(TTarget target, MethodBase methodInfo)
-        {
-            var parameters = methodInfo.GetParameters();
-            var values = new List<object>();
-            foreach (var param in parameters)
-            {
-                var binding = BindingSet.Find(param);
-                values.Add(GetParameterValue(target, context, param, binding));
-            }
-            return values.ToArray();
-        }
-
-        private object GetParameterValue(TTarget target, InvocationContext context, ParameterInfo param, BindingBase binding)
-        {
-            if (binding != null)
-            {
-                return ValueFromBinding(target, binding);
-            }
-            var value = context.ServiceProvider.GetService(param.ParameterType);
-            return value ?? (param.HasDefaultValue
-                                ? param.DefaultValue
-                                : param.ParameterType.GetDefaultValue());
-        }
-
-        private object ValueFromBinding(TTarget target, BindingBase binding)
-        {
-            object value;
-            switch (binding)
-            {
-                case FuncBinding<TTarget> funcBinding:
-                    value = funcBinding.ValueFunc(context, target);
-                    break;
-                case SymbolBinding symbolBinding:
-                    switch (symbolBinding.Symbol)
-                    {
-                        case Option option:
-                            value = context.ParseResult.GetValue(option);
-                            break;
-                        case Argument argument:
-                            value = context.ParseResult.GetValue(argument);
-                            break;
-                        default:
-                            throw new InvalidOperationException("Internal: Unknown symbol type encountered");
-                    }
-                    break;
-                default:
-                    throw new InvalidOperationException("Internal: Symbol or value func missing in BindAction");
-            }
-            // TODO: Handle conversion errors - needs to propagate so needs thought
-            var typedValue = Convert.ChangeType(value, binding.ReturnType);
-            return typedValue;
-        }
-
-        private void SetStaticProperties()
-        {
-            var bindingFlags = CommonBindingFlags | BindingFlags.Static;
-            var type = typeof(TTarget);
-            var staticProperties = type.GetProperties(bindingFlags);
-            SetProperties(null, staticProperties);
-            if (type.IsNested)
-            {
-                // TODO: How do you find a nested types parent?
-                // TODO: Set nested parents static properties
-            }
-        }
-
-        private void SetProperties(object target, IEnumerable<PropertyInfo> properties)
-        {
-            foreach (var prop in properties)
-            {
-                var binding = BindingSet.Find(prop);
-                object value = binding == null
-                                ? context.ServiceProvider.GetService(prop.PropertyType)
-                                : ValueFromBinding(null, binding);
-                prop.SetValue(target, value);
-            }
-        }
-    }
+ }
 }
