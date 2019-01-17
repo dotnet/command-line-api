583ddb2979479ccb80c3e8658a926a308393bd81
Jered Myers <jeredamyers@gmail.com>
2019-01-17 07:02:51 -0800

    Remove unnecessary string concatenation

--- src/System.CommandLine/ValidationMessages.cs
+++ src/System.CommandLine/ValidationMessages.cs
@@ -44,9 +44,8 @@ namespace System.CommandLine
                 ? $"Required argument missing for command: {symbolResult.Token}"
                 : $"Required argument missing for option: {symbolResult.Token}";
 
-        public virtual string RequiredArgumentNameMissing(string symbolAlias) =>
-            $"Name must be set for arguments with an arity above zero. "
-            + $"The symbol with the argument missing a name has the alias '{symbolAlias}'.";
+        public virtual string RequiredArgumentNameMissing(string argumentAlias) =>
+            $"Name must be set for arguments with an arity above zero. The argument missing a name has the alias '{argumentAlias}'.";
 
         public virtual string RequiredCommandWasNotProvided() =>
             "Required command was not provided.";
