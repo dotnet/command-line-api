efd115d49c51541840c91fd68fcc799e926ec56a
Jon Sequeira <jonsequeira@gmail.com>
2019-01-23 22:07:30 -0800

    R: remove unused usings

--- src/System.CommandLine.Tests/ParserTests.cs
+++ src/System.CommandLine.Tests/ParserTests.cs
@@ -2,10 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
 using System.IO;
 using FluentAssertions;
 using FluentAssertions.Equivalency;
 using System.Linq;
+using System.Threading.Tasks;
 using FluentAssertions.Common;
 using Xunit;
 using Xunit.Abstractions;
