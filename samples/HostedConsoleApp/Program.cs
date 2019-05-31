4044545d4f2ba472fba6fecfa277c2696a9a5bc5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-05-31 04:04:00 +0200

    Add CommandLine API extensions for Microsoft.Extensions.Hosting

---
+++ samples/HostedConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HostedConsoleApp
+{
+    public static class Program
+    {
+        public static Task Main(string[] args) => new CommandLineBuilder(
+            new RootCommand
+            {
+                Handler = CommandHandler.Create(typeof(Program).GetMethod(nameof(Run), BindingFlags.Static | BindingFlags.NonPublic)),
+                TreatUnmatchedTokensAsErrors = false
+            })
+            .AddOption(new Option("foo", argument: new Argument<string>()))
+            .UseDefaults()
+            .UseHost(Host.CreateDefaultBuilder)
+            .Build().InvokeAsync(args);
+
+        internal static void Run(IHost host, string foo)
+        {
+            var logger = host.Services.GetService<ILoggerFactory>()?.CreateLogger(typeof(Program))
+                ?? Microsoft.Extensions.Logging.Abstractions.NullLogger.Instance;
+
+            logger.LogInformation($"Command-line argument {nameof(foo)} supplied: {{{nameof(foo)}}}", foo);
+        }
+    }
+}
---
+++ src/System.CommandLine.Hosting/HostingExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.Linq;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace System.CommandLine.Hosting
+{
+    public static class HostingExtensions
+    {
+        public static CommandLineBuilder UseHost(this CommandLineBuilder builder,
+            Func<string[], IHostBuilder> hostBuilderFactory,
+            Action<IHostBuilder> configureHost = null) =>
+            builder.UseMiddleware(async (invocation, next) =>
+            {
+                var argsRemaining = invocation.ParseResult.UnmatchedTokens.ToArray();
+                var hostBuilder = hostBuilderFactory?.Invoke(argsRemaining)
+                    ?? new HostBuilder();
+
+                hostBuilder.ConfigureServices(services =>
+                {
+                    services.AddSingleton(invocation);
+                    services.AddSingleton(invocation.BindingContext);
+                    services.AddSingleton(invocation.Console);
+                    services.AddTransient(_ => invocation.InvocationResult);
+                    services.AddTransient(_ => invocation.ParseResult);
+                });
+                hostBuilder.UseConsoleLifetime();
+                configureHost?.Invoke(hostBuilder);
+
+                using (var host = hostBuilder.Build())
+                {
+                    invocation.BindingContext.AddService(typeof(IHost), () => host);
+
+                    await host.StartAsync();
+
+                    await next(invocation);
+
+                    await host.StopAsync();
+                }
+            });
+
+        public static CommandLineBuilder UseHost(this CommandLineBuilder builder,
+            Action<IHostBuilder> configureHost = null
+            ) => UseHost(builder, null, configureHost);
+    }
+}
