370323c6af6603f1713c29b7c29b50035ddebf16
Xavier John <xavierjohn@hotmail.com>
2019-06-11 09:54:27 -0700

    Remove unused items.

--- src/System.CommandLine.Rendering/CursorControlSpan.cs
+++ src/System.CommandLine.Rendering/CursorControlSpan.cs
@@ -8,23 +8,13 @@ namespace System.CommandLine.Rendering
     [DebuggerDisplay("{" + nameof(Name) + "}")]
     public class CursorControlSpan : Span
     {
-        public CursorControlSpan(string name, int row = 0, int column = 0)
+        public CursorControlSpan(string name)
         {
             Name = name;
-            Row = row;
-            Column = column;
         }
         public override int ContentLength => 0;
 
         public string Name { get; }
-        public int Row { get; }
-        public int Column { get; }
-
-        public static CursorControlSpan Up() => new CursorControlSpan(nameof(Up));
-        public static CursorControlSpan Down() => new CursorControlSpan(nameof(Down));
-        public static CursorControlSpan Left() => new CursorControlSpan(nameof(Left));
-        public static CursorControlSpan Right() => new CursorControlSpan(nameof(Right));
-        public static CursorControlSpan Position(int row, int column) => new CursorControlSpan(nameof(Position), row, column);
         public static CursorControlSpan Hide() => new CursorControlSpan(nameof(Hide));
         public static CursorControlSpan Show() => new CursorControlSpan(nameof(Show));
 
