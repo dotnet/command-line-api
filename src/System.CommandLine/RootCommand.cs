9ffdcbd279194667cb184281c18128ef68bdbcea
Christian Rondeau <christian.rondeau@gmail.com>
2019-08-20 18:46:34 -0400

    Fallback to args[0] when no assembly location (#617)

--- src/System.CommandLine/RootCommand.cs
+++ src/System.CommandLine/RootCommand.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace System.CommandLine
@@ -22,13 +23,20 @@ namespace System.CommandLine
             }
         }
 
-        private static readonly Lazy<string> executablePath =
-            new Lazy<string>(() =>
-                                 GetAssembly().Location);
+        private static readonly Lazy<string> executablePath = new Lazy<string>(() =>
+        {
+            return GetAssembly().Location;
+        });
 
-        private static readonly Lazy<string> executableName =
-            new Lazy<string>(() =>
-                                 Path.GetFileNameWithoutExtension(GetAssembly().Location));
+        private static readonly Lazy<string> executableName = new Lazy<string>(() =>
+        {
+            var location = executablePath.Value;
+            if (string.IsNullOrEmpty(location))
+            {
+                location = Environment.GetCommandLineArgs().FirstOrDefault();
+            }
+            return Path.GetFileNameWithoutExtension(location);
+        });
 
         private static Assembly GetAssembly() =>
             Assembly.GetEntryAssembly() ??
