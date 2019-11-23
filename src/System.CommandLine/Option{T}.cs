0de50a17b262835d90ac18e4c7424e45427714a7
Jon Sequeira <jonsequeira@gmail.com>
2019-11-23 10:29:50 -0800

    add Option<T>

---
+++ src/System.CommandLine/Option{T}.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.CommandLine
+{
+    public class Option<T> : Option
+    {
+        public Option(string alias, string description = null) : base(alias, description)
+        {
+            Argument = new Argument<T>();
+        }
+
+        public Option(string[] aliases, string description = null) : base(aliases, description)
+        {
+            Argument = new Argument<T>();
+        }
+
+        public override Argument Argument
+        {
+            set
+            {
+                if (!(value is Argument<T>))
+                {
+                    throw new ArgumentException($"{nameof(Argument)} must be of type {typeof(Argument<T>)} but was {value?.GetType().ToString() ?? "null"}");
+                }
+
+                base.Argument = value;
+            }
+        }
+    }
+}
