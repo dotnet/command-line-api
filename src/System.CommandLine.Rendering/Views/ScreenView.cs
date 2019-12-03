aa47d3ef3f769438b779f42af31bea89069d84db
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-12-03 12:13:52 +0100

    Fix NRE in ScreenView.Render when there is no terminal

--- src/System.CommandLine.Rendering/Views/ScreenView.cs
+++ src/System.CommandLine.Rendering/Views/ScreenView.cs
@@ -68,7 +68,7 @@ namespace System.CommandLine.Rendering.Views
         // may not want this?
         public void Render(Region region)
         {
-            _console.GetTerminal().HideCursor();
+            _console.GetTerminal()?.HideCursor();
 
             Child?.Render(Renderer, region);
         }
@@ -82,7 +82,7 @@ namespace System.CommandLine.Rendering.Views
 
         public void Dispose()
         {
-            _console.GetTerminal().ShowCursor();
+            _console.GetTerminal()?.ShowCursor();
 
             if (_child is View child)
             {
