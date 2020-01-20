b4fc1c715396f9083593814d24524f9735275789
Kathleen Dollard <kdollard@microsoft.com>
2020-01-20 13:00:25 -0800

    Xml comments: helping discovery of Invoke

--- src/System.CommandLine/Command.cs
+++ src/System.CommandLine/Command.cs
@@ -9,32 +9,72 @@ using System.Linq;
 
 namespace System.CommandLine
 {
+    /// <summary>
+    /// Represents a command or subcommand.
+    /// </summary>
     public class Command : Symbol, ICommand, IEnumerable<Symbol>
     {
+        /// <summary>
+        /// Creates a new command
+        /// </summary>
+        /// <param name="name">The name of the command.</param>
+        /// <param name="description">The description of the command. This will appear in help.</param>
         public Command(string name, string description = null) : base(new[] { name }, description)
         {
         }
 
+        /// <summary>
+        /// A list of the Arguments that have been defined for the command.
+        /// </summary>
         public IEnumerable<Argument> Arguments => Children.OfType<Argument>();
-        
+
+        /// <summary>
+        /// A list of the Options that have been defined for the command
+        /// </summary>
         public IEnumerable<Option> Options => Children.OfType<Option>();
 
+        /// <summary>
+        /// Add a new argument to the command.
+        /// </summary>
+        /// <param name="argument">The argument to add.</param>
         public void AddArgument(Argument argument) => AddArgumentInner(argument);
 
+        /// <summary>
+        /// Add a new subcommand to the command.
+        /// </summary>
+        /// <param name="command">The command to add.</param>
         public void AddCommand(Command command) => AddSymbol(command);
 
+        /// <summary>
+        /// Add a new option to the command.
+        /// </summary>
+        /// <param name="option">The option to add.</param>
         public void AddOption(Option option) => AddSymbol(option);
 
+        /// <summary>
+        /// Add a new command, option or argument to the command.
+        /// </summary>
+        /// <param name="symbol">The command, option or argument to add.</param>
         public void Add(Symbol symbol) => AddSymbol(symbol);
 
         public void Add(Argument argument) => AddArgument(argument);
 
         internal List<ValidateSymbol<CommandResult>> Validators { get; } = new List<ValidateSymbol<CommandResult>>();
 
+        /// <summary>
+        /// Add a delegate that does custom validation for a command. 
+        /// </summary>
+        /// <param name="validate">The delegate that contains the custom validation.</param>
         public void AddValidator(ValidateSymbol<CommandResult> validate) => Validators.Add(validate);
 
+        /// <summary>
+        /// If this value is true, extra tokens will be reported as an error.
+        /// </summary>
         public bool TreatUnmatchedTokensAsErrors { get; set; } = true;
 
+        /// <summary>
+        /// The action to perform when the command is executed. 
+        /// </summary>
         public ICommandHandler Handler { get; set; }
 
         public IEnumerator<Symbol> GetEnumerator() => Children.OfType<Symbol>().GetEnumerator();
