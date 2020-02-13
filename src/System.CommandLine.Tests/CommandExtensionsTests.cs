7911c78e39fcc76438a5998ab9c8b1b9d0850c18
Jon Sequeira <jonsequeira@gmail.com>
2020-02-13 09:55:10 -0800

    add another test

--- src/System.CommandLine.Tests/CommandExtensionsTests.cs
+++ src/System.CommandLine.Tests/CommandExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.CommandLine.Builder;
 using System.CommandLine.IO;
 using FluentAssertions;
 using Xunit;
@@ -10,7 +11,7 @@ namespace System.CommandLine.Tests
     public class CommandExtensionsTests
     {
         [Fact]
-        public void Mulitple_invocations_via_Invoke_extension_will_not_reconfigure_implicit_parser()
+        public void Command_Invoke_can_be_called_more_than_once_for_the_same_command()
         {
             var command = new RootCommand("Root command description")
             {
@@ -22,12 +23,34 @@ namespace System.CommandLine.Tests
             command.Invoke("-h", console1);
 
             console1.Out.ToString().Should().Contain(command.Description);
-
+            
             var console2 = new TestConsole();
 
             command.Invoke("-h", console2);
 
             console2.Out.ToString().Should().Contain(command.Description);
+        }
+
+        [Fact]
+        public void When_CommandLineBuilder_is_used_then_Command_Invoke_uses_its_configuration()
+        {
+            var command = new RootCommand();
+
+            new CommandLineBuilder(command)
+                .UseMiddleware(context =>
+                {
+                    context.Console.Out.Write("hello!");
+                })
+                .Build();
+
+            var console = new TestConsole();
+
+            command.Invoke("", console);
+
+            console.Out
+                   .ToString()
+                   .Should()
+                   .Contain("hello!");
         }
     }
 }
