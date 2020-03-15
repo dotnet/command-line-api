9820baf4ee301873c506b9f763673265182f1ee4
Jon Sequeira <jonsequeira@gmail.com>
2020-03-15 09:24:51 -0700

    fix #817

---
+++ src/System.CommandLine.Tests/UseParseErrorReportingTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using FluentAssertions;
+using Xunit;
+
+namespace System.CommandLine.Tests
+{
+    public class UseParseErrorReportingTests
+    {
+        [Fact] // https://github.com/dotnet/command-line-api/issues/817
+        public void Parse_error_reporting_reports_error_when_help_is_used_and_required_subcommand_is_missing()
+        {
+            var root = new RootCommand
+            {
+                new Command("inner")
+            };
+
+            var parser = new CommandLineBuilder(root)
+                         .UseParseErrorReporting()
+                         .UseHelp()
+                         .Build();
+
+            var parseResult = parser.Parse("");
+
+            parseResult.Errors.Should().NotBeEmpty();
+
+            var result = parser.Invoke("");
+
+            result.Should().Be(1);
+        }
+    }
+}
