a409f2443ff747961c8885e4bbb5192b5ecba81f
Jon Sequeira <jonsequeira@gmail.com>
2020-04-13 09:10:52 -0700

    fix nullability

--- src/System.CommandLine/Invocation/ServiceProvider.cs
+++ src/System.CommandLine/Invocation/ServiceProvider.cs
@@ -13,11 +13,11 @@ namespace System.CommandLine.Invocation
 {
     internal class ServiceProvider : IServiceProvider
     {
-        private readonly Dictionary<Type, Func<IServiceProvider, object>> _services;
+        private readonly Dictionary<Type, Func<IServiceProvider, object?>> _services;
 
         public ServiceProvider(BindingContext bindingContext)
         {
-            _services = new Dictionary<Type, Func<IServiceProvider, object>>
+            _services = new Dictionary<Type, Func<IServiceProvider, object?>>
                         {
                             [typeof(ParseResult)] = _ => bindingContext.ParseResult,
                             [typeof(IConsole)] = _ => bindingContext.Console,
@@ -29,18 +29,18 @@ namespace System.CommandLine.Invocation
 
         public void AddService<T>(Func<IServiceProvider, T> factory) => _services[typeof(T)] = p => factory(p)!;
 
-        public void AddService(Type serviceType, Func<IServiceProvider, object> factory) => _services[serviceType] = factory;
+        public void AddService(Type serviceType, Func<IServiceProvider, object?> factory) => _services[serviceType] = factory;
 
         public IReadOnlyCollection<Type> AvailableServiceTypes => _services.Keys;
 
-        public object GetService(Type serviceType)
+        public object? GetService(Type serviceType)
         {
             if (_services.TryGetValue(serviceType, out var factory))
             {
                 return factory(this);
             }
 
-            return null!;
+            return null;
         }
     }
 }
