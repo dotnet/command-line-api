4355046940707ffac4c1f151f92c02d3c7ed0a8a
NikiforovAll <nikiforovalekcey@gmail.com>
2020-06-07 22:04:53 +0300

    Add copyright header

--- src/System.CommandLine.Hosting/DefaultBuilder/GenericHostBuilderExtensions.cs
+++ src/System.CommandLine.Hosting/DefaultBuilder/GenericHostBuilderExtensions.cs
@@ -1,6 +1,8 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.CommandLine.Builder;
 using Microsoft.Extensions.DependencyInjection;
-// using System.CommandLine.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace System.CommandLine.Hosting
