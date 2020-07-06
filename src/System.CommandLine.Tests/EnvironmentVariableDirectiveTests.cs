047eed193dbd7e5b65f5c48190b1b364884a31b5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-07-06 15:08:56 +0200

    Add tests for special environment directive cases

--- src/System.CommandLine.Tests/EnvironmentVariableDirectiveTests.cs
+++ src/System.CommandLine.Tests/EnvironmentVariableDirectiveTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -82,6 +83,101 @@ namespace System.CommandLine.Tests
                 .Build();
 
             await parser.InvokeAsync(new[] { $"[env:{variable}=    {value}     ]" });
+
+            Assert.True(asserted);
+        }
+
+        [Fact]
+        public static async Task Sets_environment_variable_value_containing_equals_sign()
+        {
+            bool asserted = false;
+            string variable = $"TEST_ENVIRONMENT_VARIABLE{randomizer.Next()}";
+            const string value = "This is = a test containing equals";
+            var rootCommand = new RootCommand
+            {
+                Handler = CommandHandler.Create(() =>
+                {
+                    asserted = true;
+                    Assert.Equal(value, Environment.GetEnvironmentVariable(variable));
+                })
+            };
+
+            var parser = new CommandLineBuilder(rootCommand)
+                .UseEnvironmentVariableDirective()
+                .Build();
+
+            await parser.InvokeAsync(new[] { $"[env:{variable}={value}]" });
+
+            Assert.True(asserted);
+        }
+
+        [Fact]
+        public static async Task Ignores_environment_directive_without_equals_sign()
+        {
+            bool asserted = false;
+            string variable = $"TEST_ENVIRONMENT_VARIABLE{randomizer.Next()}";
+            var rootCommand = new RootCommand
+            {
+                Handler = CommandHandler.Create(() =>
+                {
+                    asserted = true;
+                    Assert.Null(Environment.GetEnvironmentVariable(variable));
+                })
+            };
+
+            var parser = new CommandLineBuilder(rootCommand)
+                .UseEnvironmentVariableDirective()
+                .Build();
+
+            await parser.InvokeAsync(new[] { $"[env:{variable}]" });
+
+            Assert.True(asserted);
+        }
+
+        [Fact]
+        public static async Task Ignores_environment_directive_with_empty_variable_name()
+        {
+            bool asserted = false;
+            string value = $"This is a test, random: {randomizer.Next()}";
+            var rootCommand = new RootCommand
+            {
+                Handler = CommandHandler.Create(() =>
+                {
+                    asserted = true;
+                    var env = Environment.GetEnvironmentVariables();
+                    Assert.DoesNotContain(value, env.Values.Cast<string>().ToArray());
+                })
+            };
+
+            var parser = new CommandLineBuilder(rootCommand)
+                .UseEnvironmentVariableDirective()
+                .Build();
+
+            await parser.InvokeAsync(new[] { $"[env:={value}]" });
+
+            Assert.True(asserted);
+        }
+
+        [Fact]
+        public static async Task Ignores_environment_directive_with_whitespace_variable_name()
+        {
+            bool asserted = false;
+            string value = $"This is a test, random: {randomizer.Next()}";
+            var rootCommand = new RootCommand
+            {
+                Handler = CommandHandler.Create(() =>
+                {
+                    asserted = true;
+                    var env = Environment.GetEnvironmentVariables();
+                    Assert.DoesNotContain(value, env.Values.Cast<string>().ToArray());
+                })
+            };
+
+            var parser = new CommandLineBuilder(rootCommand)
+                .UseEnvironmentVariableDirective()
+                .Build();
+
+            await parser.InvokeAsync(new[] { $"[env:    ={value}]" });
 
             Assert.True(asserted);
         }
