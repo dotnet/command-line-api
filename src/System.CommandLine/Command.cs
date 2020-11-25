18848885004728ef3e15f6dbf612b400296bdb45
Pieter Venter <pieterventer@geotab.com>
2020-11-24 22:20:33 -0500

    Add XML documentation to the Command class
    Fix #130

--- src/System.CommandLine/Command.cs
+++ src/System.CommandLine/Command.cs
@@ -10,6 +10,14 @@ using System.Linq;
 
 namespace System.CommandLine
 {
+    /// <summary>
+    /// Represents a specific action that the application performs.
+    /// </summary>
+    /// <remarks>
+    /// Use the Command object for actions that correspond to a specific string (the command name). See
+    /// <see cref="RootCommand"/> for simple applications that do only have one action. For example, <c>dotnet run</>
+    /// uses <c>run</c> as the command.
+    /// </remarks>
     public class Command : 
         NamedSymbol, 
         ICommand, 
@@ -17,33 +25,68 @@ namespace System.CommandLine
     {
         private readonly SymbolSet _globalOptions = new SymbolSet();
 
+        /// <summary>
+        /// Initializes a new instance of the Command class.
+        /// </summary>
+        /// <param name="name">The name of the command.</param>
+        /// <param name="description">The description of the command, shown in help.</param>
         public Command(string name, string? description = null) : base(name, description)
         {
         }
 
+        /// <summary>
+        /// Represents all the arguments associated with the command.
+        /// </summary>
         public IEnumerable<Argument> Arguments => 
             Children.OfType<Argument>();
 
+        /// <summary>
+        /// Represents all the options associated with the command, including global options.
+        /// </summary>
         public IEnumerable<Option> Options =>
             Children.OfType<Option>()
                     .Concat(Parents
                             .OfType<Command>()
                             .SelectMany(c => c.GlobalOptions));
-
+        /// <summary>
+        /// Represents all the global options associated with the command
+        /// </summary>
         public IEnumerable<Option> GlobalOptions => _globalOptions.OfType<Option>();
 
+        /// <summary>
+        /// Adds an <see cref="Argument"/> to the command.
+        /// </summary>
+        /// <param name="argument">The argument to add to the command.</param>
         public void AddArgument(Argument argument) => AddArgumentInner(argument);
 
+        /// <summary>
+        /// Adds a subcommand to the command. Commands can be nested to an arbitrary depth.
+        /// </summary>
+        /// <param name="command">The subcommand to add to the command.</param>
         public void AddCommand(Command command) => AddSymbol(command);
 
+        /// <summary>
+        /// Adds an <see cref="Option"/> to the command.
+        /// </summary>
+        /// <param name="option">The option to add to the command.</param>
         public void AddOption(Option option) => AddSymbol(option);
 
+        /// <summary>
+        /// Adds a global <see cref="Option"/> to the command. Global options are applied to all commands.
+        /// </summary>
+        /// <param name="option">The global option to add to the command.</param>
         public void AddGlobalOption(Option option)
         {
             _globalOptions.Add(option);
             Children.AddWithoutAliasCollisionCheck(option);
         }
-
+        
+        /// <summary>
+        /// Adds a global <see cref="Option"/> to the command. Global options are applied to all commands. A
+        /// return value indicates if the option alias is already in use.
+        /// </summary>
+        /// <param name="option">The global option to add to the command.</param>
+        /// <returns><c>true</c> if the option was added;<c>false</c> if it was already in use.</returns>
         public bool TryAddGlobalOption(Option option)
         {
             if (!_globalOptions.IsAnyAliasInUse(option, out _))
@@ -58,10 +101,23 @@ namespace System.CommandLine
             }
         }
 
+        /// <summary>
+        /// Adds a <see cref="Symbol"/> to the command.
+        /// </summary>
+        /// <param name="symbol">The symbol to add to the command.</param>
         public void Add(Symbol symbol) => AddSymbol(symbol);
 
+        /// <summary>
+        /// Adds an <see cref="Argument"/> to the command.
+        /// </summary>
+        /// <param name="argument">The argument to add to the command.</param>
         public void Add(Argument argument) => AddArgument(argument);
 
+        /// <summary>
+        /// Adds an alias to the command. Multiple aliases can be added to a command, most often used to provide a
+        /// shorthand alternative.
+        /// </summary>
+        /// <param name="alias">A string representing the alias to add to the command.</param>
         public virtual void AddAlias(string alias) => AddAliasInner(alias);
 
         private protected override void AddAliasInner(string alias)
@@ -87,12 +143,28 @@ namespace System.CommandLine
 
         internal List<ValidateSymbol<CommandResult>> Validators { get; } = new List<ValidateSymbol<CommandResult>>();
 
+        /// <summary>
+        /// Adds a custom <see cref="ValidateSymbol{T}(CommandResult)"/> to the command. Validators can be used
+        /// to create custom validation logic.
+        /// </summary>
+        /// <param name="validate">The delegate to validate the symbols during parsing.</param>
         public void AddValidator(ValidateSymbol<CommandResult> validate) => Validators.Add(validate);
 
+        /// <summary>
+        /// Gets or sets a value that indicates whether unmatched tokens should be treated as errors. For example,
+        /// if set to <c>true</c> and an extra command or argument is provided, validation will fail.
+        /// </summary>
         public bool TreatUnmatchedTokensAsErrors { get; set; } = true;
 
+        /// <summary>
+        /// Gets or sets the <see cref="ICommandHandler"/> for the command. The handler represents the action
+        /// that will be performed when the command is invoked.
+        /// </summary>
         public ICommandHandler? Handler { get; set; }
 
+        /// <summary>
+        /// Represents all the symbols associated with the command.
+        /// </summary>
         public IEnumerator<Symbol> GetEnumerator() => Children.OfType<Symbol>().GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
