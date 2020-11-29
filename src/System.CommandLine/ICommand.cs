d0650000f051a5482afc235b04682e76a3029902
Pieter Venter <pieterventer@geotab.com>
2020-11-29 10:27:03 -0500

    Add XML documentation to the ICommand interface
    Fix #130

--- src/System.CommandLine/ICommand.cs
+++ src/System.CommandLine/ICommand.cs
@@ -5,12 +5,24 @@ using System.Collections.Generic;
 
 namespace System.CommandLine
 {
+    /// <summary>
+    /// Defines a symbol with arguments and options.
+    /// </summary>
     public interface ICommand : IIdentifierSymbol
     {
+        /// <summary>
+        /// Gets a value that indicates whether unmatched tokens should be treated as errors.
+        /// </summary>
         bool TreatUnmatchedTokensAsErrors { get; }
 
+        /// <summary>
+        /// Gets the arguments for the command.
+        /// </summary>
         IEnumerable<IArgument> Arguments { get; }
         
+        /// <summary>      `
+        /// Gets the options for the command.
+        /// </summary>
         IEnumerable<IOption> Options { get; }
     }
 }
