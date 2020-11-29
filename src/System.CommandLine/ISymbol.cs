625257401514445541136f088c7654bd88e3dcb1
Pieter Venter <pieterventer@geotab.com>
2020-11-29 10:05:06 -0500

    Add XML documentation to the ISymbol interface
    Fix #130

--- src/System.CommandLine/ISymbol.cs
+++ src/System.CommandLine/ISymbol.cs
@@ -6,16 +6,34 @@ using System.CommandLine.Suggestions;
 
 namespace System.CommandLine
 {
+    /// <summary>
+    /// Defines a named symbol that resides in a hierarchy with parent and child symbols.
+    /// </summary>
     public interface ISymbol : ISuggestionSource
     {
+        /// <summary>
+        /// Gets the symbol name.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Gets the symbol description.
+        /// </summary>
         string? Description { get; }
 
+        /// <summary>
+        /// Gets a value that indicates whether the symbol is hidden.
+        /// </summary>
         bool IsHidden { get; }
 
+        /// <summary>
+        /// Gets the child symbols.
+        /// </summary>
         ISymbolSet Children { get; }
 
+        /// <summary>
+        /// Gets the parent symbols.
+        /// </summary>
         ISymbolSet Parents { get; }
     }
 }
