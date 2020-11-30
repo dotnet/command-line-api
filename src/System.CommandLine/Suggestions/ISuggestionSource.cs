105808a23fa3e0164af18e3eca1b51ecfb7682bd
Pieter Venter <pieterventer@geotab.com>
2020-11-29 21:58:34 -0500

    Add XML documentation to the isuggestionsource interface

--- src/System.CommandLine/Suggestions/ISuggestionSource.cs
+++ src/System.CommandLine/Suggestions/ISuggestionSource.cs
@@ -6,8 +6,17 @@ using System.CommandLine.Parsing;
 
 namespace System.CommandLine.Suggestions
 {
+    /// <summary>
+    /// Defines the functionality to suggest values.
+    /// </summary>
     public interface ISuggestionSource
     {
+        /// <summary>
+        /// Gets the suggested values for the given parse result and input text.
+        /// </summary>
+        /// <param name="parseResult">The result provided by the parser.</param>
+        /// <param name="textToMatch">The input text to match on.</param>
+        /// <returns>Strings that provide suggested values to the user.</returns>
         IEnumerable<string?> GetSuggestions(ParseResult? parseResult = null, string? textToMatch = null);
     }
 }
