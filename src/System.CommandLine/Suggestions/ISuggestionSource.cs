5bb3a0aeeb469d75bdd70ca959114e133eccd771
pieter-venter <pieterventer@geotab.com>
2020-12-09 19:54:32 -0500

    XML documentation ISuggestionSource interface (#1109)
    * Add XML documentation to the isuggestionsource interface
    
    * Add suggested changes.

--- src/System.CommandLine/Suggestions/ISuggestionSource.cs
+++ src/System.CommandLine/Suggestions/ISuggestionSource.cs
@@ -6,8 +6,17 @@ using System.CommandLine.Parsing;
 
 namespace System.CommandLine.Suggestions
 {
+    /// <summary>
+    /// Provides suggestions for tab completion and example values for help.
+    /// </summary>
     public interface ISuggestionSource
     {
+        /// <summary>
+        /// Gets the suggested values for the given parse result and input text.
+        /// </summary>
+        /// <param name="parseResult">The result provided by the parser.</param>
+        /// <param name="textToMatch">The input text to match on.</param>
+        /// <returns>Strings that provide suggested values to the user.</returns>
         IEnumerable<string?> GetSuggestions(ParseResult? parseResult = null, string? textToMatch = null);
     }
 }
