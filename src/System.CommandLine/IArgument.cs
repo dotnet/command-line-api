b0e07d156e966f509e28be0ebca2e52af0c3a668
Jon Sequeira <jonsequeira@gmail.com>
2020-12-11 09:17:58 -0800

    R: remove redundant interface

--- src/System.CommandLine/IArgument.cs
+++ src/System.CommandLine/IArgument.cs
@@ -2,13 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.CommandLine.Binding;
-using System.CommandLine.Suggestions;
 
 namespace System.CommandLine
 {
     public interface IArgument : 
         ISymbol,
-        ISuggestionSource, 
         IValueDescriptor
     {
         IArgumentArity Arity { get; }
