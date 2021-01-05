2557b0c9b7f2173831f779e5aa491ac9e7b828c2
Jon Sequeira <jonsequeira@gmail.com>
2021-01-05 08:28:29 -0800

    fix #1135

---
+++ src/System.CommandLine/SuggestionSourceList.cs
@@ -0,0 +1,32 @@
+﻿// // Copyright (c) .NET Foundation and contributors. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections;
+using System.Collections.Generic;
+using System.CommandLine.Suggestions;
+
+namespace System.CommandLine
+{
+    public class SuggestionSourceList : IReadOnlyList<ISuggestionSource>
+    {
+        private readonly List<ISuggestionSource> _sources = new List<ISuggestionSource>();
+
+        public void Add(ISuggestionSource source)
+        {
+            _sources.Add(source);
+        }
+
+        public IEnumerator<ISuggestionSource> GetEnumerator() => _sources.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Clear()
+        {
+            _sources.Clear();
+        }
+
+        public int Count => _sources.Count;
+
+        public ISuggestionSource this[int index] => _sources[index];
+    }
+}
