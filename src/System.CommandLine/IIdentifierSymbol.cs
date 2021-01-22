14a248d535276a23b6e80b8839ecf8f73de3d70a
pieter-venter <pieterventer@geotab.com>
2021-01-22 11:38:07 -0500

    XML documentation for IIdentifierSymbol interface (#1104)
    * Add XML documentation to the IIdentifierSymbol interface
    
    * Improve wording

--- src/System.CommandLine/IIdentifierSymbol.cs
+++ src/System.CommandLine/IIdentifierSymbol.cs
@@ -5,10 +5,21 @@ using System.Collections.Generic;
 
 namespace System.CommandLine
 {
+    /// <summary>
+    /// Defines a symbol with aliases.
+    /// </summary>
     public interface IIdentifierSymbol : ISymbol
     {
+        /// <summary>
+        /// Gets the aliases for the symbol.
+        /// </summary>
         IReadOnlyCollection<string> Aliases { get; }
 
+        /// <summary>
+        /// Determines whether the alias has already been defined.
+        /// </summary>
+        /// <param name="alias">The alias to search for.</param>
+        /// <returns><c>true</c> if the alias has already been defined; otherwise <c>false</c>.</returns>
         bool HasAlias(string alias);
     }
 }
