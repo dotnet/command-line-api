89741990f846a494ee0dfb3a0ad25b777b72148f
pieter-venter <pieterventer@geotab.com>
2021-01-22 11:36:43 -0500

    Add XML documentation to the IOption interface (#1108)

--- src/System.CommandLine/IOption.cs
+++ src/System.CommandLine/IOption.cs
@@ -5,12 +5,24 @@ using System.CommandLine.Binding;
 
 namespace System.CommandLine
 {
+    /// <summary>
+    /// Defines a symbol with an argument. 
+    /// </summary>
     public interface IOption : IIdentifierSymbol, IValueDescriptor
     {
+        /// <summary>
+        /// Gets the argument for the option.
+        /// </summary>
         IArgument Argument { get; }
 
+        /// <summary>
+        /// Gets a value that indicates whether the option is required.
+        /// </summary>
         bool IsRequired { get; }
 
+        /// <summary>
+        /// Gets a value that indicates whether multiple argument values are allowed.
+        /// </summary>
         bool AllowMultipleArgumentsPerToken { get; }
     }
 }
