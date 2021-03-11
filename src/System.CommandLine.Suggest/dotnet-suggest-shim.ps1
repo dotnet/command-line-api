98073f298b73cd0d7f184fe961a88d76173dbc59
Staffan Gustafsson <staffangu@outlook.com>
2021-03-11 16:31:09 +0100

    Fixing confusing variable names in powershell suggest shim

--- src/System.CommandLine.Suggest/dotnet-suggest-shim.ps1
+++ src/System.CommandLine.Suggest/dotnet-suggest-shim.ps1
@@ -1,13 +1,13 @@
 # dotnet suggest shell start
 $availableToComplete = (dotnet-suggest list) | Out-String
-$availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries) 
+$availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
 
 
     Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
-        param($commandName, $wordToComplete, $cursorPosition)
-        $fullpath = (Get-Command $wordToComplete.CommandElements[0]).Source
+        param($wordToComplete, $commandAst, $cursorPosition)
+        $fullpath = (Get-Command $commandAst.CommandElements[0]).Source
 
-        $arguments = $wordToComplete.Extent.ToString().Replace('"', '\"')
+        $arguments = $commandAst.Extent.ToString().Replace('"', '\"')
         dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
         }
