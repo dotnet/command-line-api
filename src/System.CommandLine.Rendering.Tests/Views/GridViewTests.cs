2f56778bca7c67f5912f9c073058715a0b31d50c
BobSilent <Andre_1@gmx.net>
2021-05-24 21:49:46 +0200

    Add Column Padding for all SizeModes, to render all columns equal

--- src/System.CommandLine.Rendering.Tests/Views/GridViewTests.cs
+++ src/System.CommandLine.Rendering.Tests/Views/GridViewTests.cs
@@ -70,12 +70,12 @@ namespace System.CommandLine.Rendering.Tests.Views
 
             var terminal = new TestTerminal();
             var renderer = new ConsoleRenderer(terminal, outputMode);
-            grid.Render(renderer, new Region(0, 0, 20, 1));
+            grid.Render(renderer, new Region(0, 0, 22, 1));
 
             terminal.Events.Should().BeEquivalentSequenceTo(
                                                             new TestTerminal.CursorPositionChanged(new Point(0, 0)),
-                                                            new TestTerminal.ContentWritten("brown fox "),
-                                                            new TestTerminal.CursorPositionChanged(new Point(10, 0)),
+                                                            new TestTerminal.ContentWritten("brown fox"),
+                                                            new TestTerminal.CursorPositionChanged(new Point(11, 0)),
                                                             new TestTerminal.ContentWritten("jumped    "));
         }
 
@@ -112,6 +112,33 @@ namespace System.CommandLine.Rendering.Tests.Views
         [Theory]
         [InlineData(OutputMode.Ansi)]
         [InlineData(OutputMode.NonAnsi)]
+        public void Column_width_definition_is_preserved_even_defintion_is_mixed_for_subsequent_columns2(OutputMode outputMode)
+        {
+            var grid = new GridView();
+            grid.SetColumns(ColumnDefinition.Fixed(9), ColumnDefinition.Star(1), ColumnDefinition.SizeToContent(), ColumnDefinition.Fixed(15));
+            grid.SetChild(new ContentView("The quick"), 0, 0);
+            grid.SetChild(new ContentView("brown fox"), 1, 0);
+            grid.SetChild(new ContentView("jumped"), 2, 0);
+            grid.SetChild(new ContentView("over the sleepy"), 3, 0);
+
+            var terminal = new TestTerminal();
+            var renderer = new ConsoleRenderer(terminal, outputMode);
+            grid.Render(renderer, new Region(0, 0, 45, 1));
+
+            terminal.Events.Should().BeEquivalentSequenceTo(
+                                                            new TestTerminal.CursorPositionChanged(new Point(0, 0)),
+                                                            new TestTerminal.ContentWritten("The quick"),
+                                                            new TestTerminal.CursorPositionChanged(new Point(11, 0)),
+                                                            new TestTerminal.ContentWritten("brown fox"),
+                                                            new TestTerminal.CursorPositionChanged(new Point(22, 0)),
+                                                            new TestTerminal.ContentWritten("jumped"),
+                                                            new TestTerminal.CursorPositionChanged(new Point(30, 0)),
+                                                            new TestTerminal.ContentWritten("over the sleepy"));
+        }
+
+        [Theory]
+        [InlineData(OutputMode.Ansi)]
+        [InlineData(OutputMode.NonAnsi)]
         public void Column_width_definition_is_preserved_even_defintion_is_mixed_for_subsequent_columns(OutputMode outputMode)
         {
             var grid = new GridView();
@@ -123,16 +150,16 @@ namespace System.CommandLine.Rendering.Tests.Views
 
             var terminal = new TestTerminal();
             var renderer = new ConsoleRenderer(terminal, outputMode);
-            grid.Render(renderer, new Region(0, 0, 115, 1));
+            grid.Render(renderer, new Region(0, 0, 121, 1));
 
             terminal.Events.Should().BeEquivalentSequenceTo(
                 new TestTerminal.CursorPositionChanged(new Point(0, 0)),
                 new TestTerminal.ContentWritten("The quick "),
-                new TestTerminal.CursorPositionChanged(new Point(10, 0)),
+                new TestTerminal.CursorPositionChanged(new Point(12, 0)),
                 new TestTerminal.ContentWritten("brown fox" + new string(' ', 71)),
-                new TestTerminal.CursorPositionChanged(new Point(90, 0)),
+                new TestTerminal.CursorPositionChanged(new Point(94, 0)),
                 new TestTerminal.ContentWritten("jumped    "),
-                new TestTerminal.CursorPositionChanged(new Point(100, 0)),
+                new TestTerminal.CursorPositionChanged(new Point(106, 0)),
                 new TestTerminal.ContentWritten("over the sleepy"));
         }
 
@@ -151,24 +178,24 @@ namespace System.CommandLine.Rendering.Tests.Views
 
             var terminal = new TestTerminal();
             var renderer = new ConsoleRenderer(terminal, outputMode);
-            grid.Render(renderer, new Region(0, 0, 10, 4));
+            grid.Render(renderer, new Region(0, 0, 12, 4));
 
             terminal.Events.Should().BeEquivalentSequenceTo(
                 new TestTerminal.CursorPositionChanged(new Point(0, 0)),
                 new TestTerminal.ContentWritten("The  "),
                 new TestTerminal.CursorPositionChanged(new Point(0, 1)),
                 new TestTerminal.ContentWritten("quick"),
-                new TestTerminal.CursorPositionChanged(new Point(5, 0)),
+                new TestTerminal.CursorPositionChanged(new Point(7, 0)),
                 new TestTerminal.ContentWritten("brown"),
-                new TestTerminal.CursorPositionChanged(new Point(5, 1)),
+                new TestTerminal.CursorPositionChanged(new Point(7, 1)),
                 new TestTerminal.ContentWritten("fox  "),
                 new TestTerminal.CursorPositionChanged(new Point(0, 2)),
                 new TestTerminal.ContentWritten("jumpe"),
                 new TestTerminal.CursorPositionChanged(new Point(0, 3)),
                 new TestTerminal.ContentWritten("     "),
-                new TestTerminal.CursorPositionChanged(new Point(5, 2)),
+                new TestTerminal.CursorPositionChanged(new Point(7, 2)),
                 new TestTerminal.ContentWritten("over "),
-                new TestTerminal.CursorPositionChanged(new Point(5, 3)),
+                new TestTerminal.CursorPositionChanged(new Point(7, 3)),
                 new TestTerminal.ContentWritten("     "));
         }
 
@@ -187,22 +214,22 @@ namespace System.CommandLine.Rendering.Tests.Views
 
             var terminal = new TestTerminal();
             var renderer = new ConsoleRenderer(terminal, outputMode);
-            grid.Render(renderer, new Region(0, 0, 10, 4));
+            grid.Render(renderer, new Region(0, 0, 12, 4));
 
             terminal.Events
                    .Should()
                    .BeEquivalentSequenceTo(
                 new TestTerminal.CursorPositionChanged(new Point(0, 0)),
                 new TestTerminal.ContentWritten("The   "),
-                new TestTerminal.CursorPositionChanged(new Point(6, 0)),
+                new TestTerminal.CursorPositionChanged(new Point(8, 0)),
                 new TestTerminal.ContentWritten("brow"),
                 new TestTerminal.CursorPositionChanged(new Point(0, 1)),
                 new TestTerminal.ContentWritten("jumped"),
                 new TestTerminal.CursorPositionChanged(new Point(0, 2)),
                 new TestTerminal.ContentWritten("over  "),
-                new TestTerminal.CursorPositionChanged(new Point(6, 1)),
+                new TestTerminal.CursorPositionChanged(new Point(8, 1)),
                 new TestTerminal.ContentWritten("the "),
-                new TestTerminal.CursorPositionChanged(new Point(6, 2)),
+                new TestTerminal.CursorPositionChanged(new Point(8, 2)),
                 new TestTerminal.ContentWritten("slee"));
         }
 
@@ -225,11 +252,11 @@ namespace System.CommandLine.Rendering.Tests.Views
 
             terminal.Events.Should().BeEquivalentSequenceTo(
                 new TestTerminal.CursorPositionChanged(new Point(0, 0)),
-                new TestTerminal.ContentWritten("The quick    "),
+                new TestTerminal.ContentWritten("The quick  "),
                 new TestTerminal.CursorPositionChanged(new Point(13, 0)),
                 new TestTerminal.ContentWritten("brown fox "),
                 new TestTerminal.CursorPositionChanged(new Point(0, 1)),
-                new TestTerminal.ContentWritten("jumped over  "),
+                new TestTerminal.ContentWritten("jumped over"),
                 new TestTerminal.CursorPositionChanged(new Point(13, 1)),
                 new TestTerminal.ContentWritten("the sleepy"));
         }
@@ -253,15 +280,15 @@ namespace System.CommandLine.Rendering.Tests.Views
 
             terminal.Events.Should().BeEquivalentSequenceTo(
                 new TestTerminal.CursorPositionChanged(new Point(0, 0)),
-                new TestTerminal.ContentWritten("The quick    "),
+                new TestTerminal.ContentWritten("The quick  "),
                 new TestTerminal.CursorPositionChanged(new Point(0, 1)),
-                new TestTerminal.ContentWritten("             "),
+                new TestTerminal.ContentWritten("           "),
                 new TestTerminal.CursorPositionChanged(new Point(13, 0)),
                 new TestTerminal.ContentWritten("brown"),
                 new TestTerminal.CursorPositionChanged(new Point(13, 1)),
                 new TestTerminal.ContentWritten("fox  "),
                 new TestTerminal.CursorPositionChanged(new Point(0, 2)),
-                new TestTerminal.ContentWritten("jumped over  "),
+                new TestTerminal.ContentWritten("jumped over"),
                 new TestTerminal.CursorPositionChanged(new Point(13, 2)),
                 new TestTerminal.ContentWritten("the s"));
         }
