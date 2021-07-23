d183cfd78a741ff60cdbb00a659183efb4a5de0b
Kevin Bost <kitokeboo@gmail.com>
2021-07-22 20:58:54 -0700

    MVP minimum working sample....

--- System.CommandLine.CommandHandler/CommandHandlerGenerator.cs
+++ System.CommandLine.CommandHandler/CommandHandlerGenerator.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace System.CommandLine.CommandHandler
@@ -31,6 +32,7 @@ namespace System.CommandLine.Invocation
 
             builder.AppendLine("    }");
             builder.AppendLine("}");
+            builder.AppendLine("#nullable restore");
             
             context.AddSource("GeneratedCommandHandler.g.cs", builder.ToString());
         }
@@ -40,7 +42,7 @@ namespace System.CommandLine.Invocation
 #if DEBUG
             if (!System.Diagnostics.Debugger.IsAttached)
             {
-                System.Diagnostics.Debugger.Launch();
+                //System.Diagnostics.Debugger.Launch();
             }
 #endif 
             
@@ -58,16 +60,67 @@ namespace System.CommandLine.Invocation
                 invocation.Expression is MemberAccessExpressionSyntax memberAccess &&
                 memberAccess.Name.Identifier.Text == "Generate")
             {
-                SymbolInfo symbol = context.SemanticModel.GetSymbolInfo(invocation);
-                SymbolInfo delegateType = context.SemanticModel.GetSymbolInfo(invocation.ArgumentList.Arguments[0].Expression);
-                if (delegateType.Symbol is IMethodSymbol methodSymbol)
-                {
+                //SymbolInfo symbol = context.SemanticModel.GetSymbolInfo(invocation);
+                //SymbolInfo delegateType = context.SemanticModel.GetSymbolInfo(invocation.ArgumentList.Arguments[0].Expression);
+                //if (delegateType.Symbol is IMethodSymbol methodSymbol)
+                //{
+                //
+                //}
+                List<ISymbol?> symbols = invocation.ArgumentList.Arguments
+                    .Skip(1)
+                    .Select(x => context.SemanticModel.GetSymbolInfo(x.Expression).Symbol)
+                    .ToList();
+                if (symbols.Any(x => x is null)) return;
 
+                string genericParameters = string.Join(", ", symbols.Select((x, i) => $"T{i + 1}"));
+                StringBuilder sb = new();
+                sb.AppendLine($"        public static ICommandHandler Generate<{genericParameters}>(Action<{string.Join(", ", symbols.Select((x, i) => $"T{i + 1}?"))}> method,");
+                for (int i = 0; i < symbols.Count; i++)
+                {
+                    var argumentSymbol = symbols[i];
+                    INamedTypeSymbol namedType;
+
+                    string FromTypeSymbol(ITypeSymbol typeSymbol)
+                    {
+                        return typeSymbol switch
+                        {
+                            INamedTypeSymbol namedType => FromNamedType(namedType),
+                            _ => typeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)
+                        };
+                    }
+
+                    string FromNamedType(INamedTypeSymbol namedType)
+                    {
+                        if (namedType.TypeArguments.Length == 1)
+                        {
+                            var displayParts = namedType.ToDisplayParts(SymbolDisplayFormat.FullyQualifiedFormat)
+                                .ToList();
+                            return string.Join("", displayParts.Take(displayParts.Count - 3)) + $"<T{i + 1}>";
+                        }
+                        return namedType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+                    }
+
+                    string type = argumentSymbol switch
+                    {
+                        ILocalSymbol local =>  FromTypeSymbol(local.Type),
+                        _ => argumentSymbol!.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)
+                    };
+
+                    sb.Append($"            {type} param{i + 1}");
+                    if (i < symbols.Count - 1)
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.AppendLine();
                 }
-                SymbolInfo paramType = context.SemanticModel.GetSymbolInfo(invocation.ArgumentList.Arguments[1].Expression);
+                sb.AppendLine("            )");
+                sb.AppendLine($"            => new GeneratedHandler<{genericParameters}>(method, {string.Join(", ", symbols.Select((_, i) => $"param{i + 1}"))});");
+
+
+                //SymbolInfo paramType = context.SemanticModel.GetSymbolInfo(invocation.ArgumentList.Arguments[1].Expression);
+                Invocations.Add(sb.ToString());
             }
             //context.Node.IsKind(Microsoft.CodeAnalysis.CSharp.SyntaxKind.)
         }
-
     }
 }
