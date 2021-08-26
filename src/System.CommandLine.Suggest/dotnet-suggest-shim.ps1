af67d42885638b929311cab7dca472ceac4cb89f
Justin Vogt <84424169+justin-vogt@users.noreply.github.com>
2021-08-26 14:17:49 -0500

    Added check for the dotnet-suggest tool installation and basic troubleshooting information.

--- src/System.CommandLine.Suggest/dotnet-suggest-shim.ps1
+++ src/System.CommandLine.Suggest/dotnet-suggest-shim.ps1
@@ -1,16 +1,24 @@
 # dotnet suggest shell start
-$availableToComplete = (dotnet-suggest list) | Out-String
-$availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
+if (Get-Command "dotnet-suggest" -errorAction SilentlyContinue)
+{
+    $availableToComplete = (dotnet-suggest list) | Out-String
+    $availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
 
 
-    Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
-        param($wordToComplete, $commandAst, $cursorPosition)
-        $fullpath = (Get-Command $commandAst.CommandElements[0]).Source
+        Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
+            param($wordToComplete, $commandAst, $cursorPosition)
+            $fullpath = (Get-Command $commandAst.CommandElements[0]).Source
 
-        $arguments = $commandAst.Extent.ToString().Replace('"', '\"')
-        dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            $arguments = $commandAst.Extent.ToString().Replace('"', '\"')
+            dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
         }
-    }
-$env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.1"
+    $env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.1"
+}
+else
+{
+    "Shell could not load the System.CommandLine suggest support - Missing [dotnet-suggest] tool."
+    "See the following for tool installation: https://www.nuget.org/packages/dotnet-suggest"
+}
 # dotnet suggest script end
