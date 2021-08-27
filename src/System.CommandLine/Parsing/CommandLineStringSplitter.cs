20addd36f2d25abf6bd075a016c6696cfd8ac92a
Kumar Sahab <krsahab@outlook.com>
2021-08-27 19:20:32 +0530

    Fix #1385
    Fix for issue #1385
    Ignore whitespace inside the quote
    Ignore quotes if seeking == Boundary.WordEnd
    Handle edge cases

--- src/System.CommandLine/Parsing/CommandLineStringSplitter.cs
+++ src/System.CommandLine/Parsing/CommandLineStringSplitter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System.Text;
 
 namespace System.CommandLine.Parsing
 {
@@ -13,6 +14,7 @@ namespace System.CommandLine.Parsing
         {
             TokenStart,
             WordEnd,
+            QuoteStart,
             QuoteEnd
         }
 
@@ -25,7 +27,8 @@ namespace System.CommandLine.Parsing
             var pos = 0;
 
             var seeking = Boundary.TokenStart;
-            int? skipQuoteAtIndex = null;
+            var seekingQuote = Boundary.QuoteStart;
+            StringBuilder sb = new StringBuilder();
 
             while (pos < memory.Length)
             {
@@ -33,44 +36,57 @@ namespace System.CommandLine.Parsing
 
                 if (char.IsWhiteSpace(c))
                 {
-                    switch (seeking)
+                    if (seekingQuote == Boundary.QuoteStart)
                     {
-                        case Boundary.WordEnd:
-                            yield return CurrentToken();
-                            startTokenIndex = pos;
-                            seeking = Boundary.TokenStart;
-                            break;
+                        switch (seeking)
+                        {
+                            case Boundary.WordEnd:
+                                yield return CurrentToken();
+                                startTokenIndex = pos;
+                                seeking = Boundary.TokenStart;
+                                break;
 
-                        case Boundary.TokenStart:
-                            startTokenIndex = pos;
-                            break;
-
-                        case Boundary.QuoteEnd:
-                            break;
+                            case Boundary.TokenStart:
+                                startTokenIndex = pos;
+                                break;
+                        }
                     }
                 }
                 else if (c == '\"')
                 {
-                    switch (seeking)
+                    if (seeking == Boundary.TokenStart)
                     {
-                        case Boundary.QuoteEnd:
-                            yield return CurrentToken();
-                            startTokenIndex = pos;
-                            seeking = Boundary.TokenStart;
-                            break;
+                        switch (seekingQuote)
+                        {
+                            case Boundary.QuoteEnd:
+                                yield return CurrentToken();
+                                startTokenIndex = pos;
+                                seekingQuote = Boundary.QuoteStart;
+                                break;
 
-                        case Boundary.TokenStart:
-                            startTokenIndex = pos + 1;
-                            seeking = Boundary.QuoteEnd;
-                            break;
+                            case Boundary.QuoteStart:
+                                startTokenIndex = pos + 1;
+                                seekingQuote = Boundary.QuoteEnd;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        switch (seekingQuote)
+                        {
+                            case Boundary.QuoteEnd:
+                                seekingQuote = Boundary.QuoteStart;
+                                AppendToken();
+                                break;
 
-                        case Boundary.WordEnd:
-                            seeking = Boundary.QuoteEnd;
-                            skipQuoteAtIndex = pos;
-                            break;
+                            case Boundary.QuoteStart:
+                                seekingQuote = Boundary.QuoteEnd;
+                                AppendToken();
+                                break;
+                        }
                     }
                 }
-                else if (seeking == Boundary.TokenStart)
+                else if (seeking == Boundary.TokenStart && seekingQuote == Boundary.QuoteStart)
                 {
                     seeking = Boundary.WordEnd;
                     startTokenIndex = pos;
@@ -93,32 +109,21 @@ namespace System.CommandLine.Parsing
 
             void Advance() => pos++;
 
+            void AppendToken()
+            {
+                sb.Append(GetToken());
+                startTokenIndex = pos + 1;
+            }
+
             string CurrentToken()
             {
-                if (skipQuoteAtIndex is null)
-                {
-                    return memory.Slice(
-                                     startTokenIndex,
-                                     IndexOfEndOfToken())
-                                 .ToString();
-                }
-                else
-                {
-                    var beforeQuote = memory.Slice(
-                        startTokenIndex,
-                        skipQuoteAtIndex.Value - startTokenIndex);
-
-                    var indexOfCharAfterQuote = skipQuoteAtIndex.Value + 1;
-
-                    var afterQuote = memory.Slice(
-                        indexOfCharAfterQuote,
-                        pos - skipQuoteAtIndex.Value - 1);
-
-                    skipQuoteAtIndex = null;
-
-                    return $"{beforeQuote}{afterQuote}";
-                }
+                sb.Append(GetToken());
+                var result = sb.ToString();
+                sb.Clear();
+                return result;
             }
+
+            string GetToken() => memory.Slice(startTokenIndex, IndexOfEndOfToken()).ToString();
 
             int IndexOfEndOfToken() => pos - startTokenIndex;
 
