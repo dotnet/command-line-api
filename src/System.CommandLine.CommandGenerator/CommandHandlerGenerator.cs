98e9eb017e8104db2854324db6db6a5d719c69e5
Kevin Bost <kitokeboo@gmail.com>
2021-09-07 14:02:42 -0700

    Downgrade C# code analysis version
    Disable nullable checks on generated code

--- src/System.CommandLine.CommandGenerator/CommandHandlerGenerator.cs
+++ src/System.CommandLine.CommandGenerator/CommandHandlerGenerator.cs
@@ -18,7 +18,7 @@ namespace System.CommandLine.CommandGenerator
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#nullable enable
+#nullable disable
 using System.CommandLine.Binding;
 using System.Reflection;
 using System.Threading.Tasks;
