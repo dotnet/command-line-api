55de6ec8eedf88eea5c1dd7e30a3413c1a057ce2
Jon Sequeira <jonsequeira@gmail.com>
2021-09-28 08:07:26 -0700

    more docs

--- src/System.CommandLine/Binding/IValueSource.cs
+++ src/System.CommandLine/Binding/IValueSource.cs
@@ -3,8 +3,18 @@
 
 namespace System.CommandLine.Binding
 {
+    /// <summary>
+    /// Binds a value from a <see cref="BindingContext"/> based on a <see cref="IValueDescriptor"/>.
+    /// </summary>
     public interface IValueSource
     {
+        /// <summary>
+        /// Gets a value from a binding context. A return value indicates whether a value matching the specified value descriptor was present.
+        /// </summary>
+        /// <param name="valueDescriptor">The descriptor for the value to be bound.</param>
+        /// <param name="bindingContext">The binding context from which to bind the value.</param>
+        /// <param name="boundValue">The bound value.</param>
+        /// <returns><see langword="true"/> if a matching value was found; otherwise, <see langword="false"/>.</returns>
         bool TryGetValue(
             IValueDescriptor valueDescriptor,
             BindingContext? bindingContext,
