f6a482398de170fc888e6986c2d8b9208be642e3
Jon Sequeira <jonsequeira@gmail.com>
2021-11-18 09:31:12 -0800

    remove dependency on Microsoft.CSharp (#1472)
    * remove dependency on Microsoft.CSharp
    
    * fix source build patch

--- src/System.CommandLine/IO/ConsoleExtensions.cs
+++ src/System.CommandLine/IO/ConsoleExtensions.cs
@@ -1,20 +1,13 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Linq;
-
 namespace System.CommandLine.IO
 {
     internal static class ConsoleExtensions
     {
         internal static void SetTerminalForegroundRed(this IConsole console)
         {
-            if (console.GetType().GetInterfaces().Any(i => i.Name == "ITerminal"))
-            {
-                ((dynamic)console).ForegroundColor = ConsoleColor.Red;
-            }
-
-            if (Platform.IsConsoleRedirectionCheckSupported && 
+            if (Platform.IsConsoleRedirectionCheckSupported &&
                 !Console.IsOutputRedirected)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -27,12 +20,7 @@ namespace System.CommandLine.IO
 
         internal static void ResetTerminalForegroundColor(this IConsole console)
         {
-            if (console.GetType().GetInterfaces().Any(i => i.Name == "ITerminal"))
-            {
-                ((dynamic)console).ForegroundColor = ConsoleColor.Red;
-            }
-
-            if (Platform.IsConsoleRedirectionCheckSupported && 
+            if (Platform.IsConsoleRedirectionCheckSupported &&
                 !Console.IsOutputRedirected)
             {
                 Console.ResetColor();
