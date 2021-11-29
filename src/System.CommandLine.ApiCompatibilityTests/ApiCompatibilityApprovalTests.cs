08c40cbc6291e6cc8c2f86137301226d3b83c26e
Jon Sequeira <jonsequeira@gmail.com>
2021-11-29 15:48:22 -0800

    API contract compatibility tests baseline

---
+++ src/System.CommandLine.ApiCompatibilityTests/ApiCompatibilityApprovalTests.cs
@@ -0,0 +1,28 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine.Parsing;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace System.CommandLine.ApiCompatibilityTests;
+
+public class ApiCompatibilityApprovalTests
+{
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void System_CommandLine_api_is_not_changed()
+    {
+        var contract = ApiContract.GenerateContractForAssembly(typeof(ParseResult).Assembly);
+        Approvals.Verify(contract);
+    }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void System_CommandLine_Hosting_api_is_not_changed()
+    {
+        var contract = ApiContract.GenerateContractForAssembly(typeof(Hosting.HostingExtensions).Assembly);
+        Approvals.Verify(contract);
+    }
+}
