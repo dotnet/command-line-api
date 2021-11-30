f7bf8805b2b1727bcb1d80664aac7f7cb538686e
Kevin Bost <kitokeboo@gmail.com>
2021-11-29 16:30:02 -0800

    Separate generator anchor to its own assembly

--- src/System.CommandLine/CommandExtensions.cs
+++ src/System.CommandLine/CommandExtensions.cs
@@ -107,23 +107,5 @@ namespace System.CommandLine
             this Command command,
             string commandLine) =>
             command.GetOrCreateDefaultParser().Parse(commandLine);
-
-        private const string _messageForWhenGeneratorIsNotInUse =
-            "This overload should not be called. You should reference the System.CommandLine.Generator package which will generate a more specific overload for your delegate.";
-
-        /// <summary>
-        /// Sets a command handler.
-        /// </summary>
-        /// <remarks>Currently, this method only works with C# source generators.</remarks>
-        /// <param name="command">The command on which to set the handler.</param>
-        /// <param name="delegate">A delegate implementing the handler for the command.</param>
-        /// <param name="symbols">The symbols used to bind the handler's parameters.</param>
-        public static void SetHandler<TDelegate>(
-            this Command command,
-            TDelegate @delegate,
-            params ISymbol[] symbols)
-        {
-            throw new InvalidOperationException(_messageForWhenGeneratorIsNotInUse);
-        }
     }
 }
