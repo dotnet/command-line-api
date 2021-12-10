5e6e61531e56ed5f506612fec602b45560982595
Adam Sitnik <adam.sitnik@gmail.com>
2021-12-10 10:42:25 +0100

    ensure Token is implementing IEquatable<Token> interface correctly

--- src/System.CommandLine/Parsing/Token.cs
+++ src/System.CommandLine/Parsing/Token.cs
@@ -1,14 +1,12 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
-
 namespace System.CommandLine.Parsing
 {
     /// <summary>
     /// A unit of significant text on the command line.
     /// </summary>
-    public class Token
+    public class Token : IEquatable<Token>
     {
         /// <param name="value">The string value of the token.</param>
         /// <param name="type">The type of the token.</param>
@@ -51,13 +49,13 @@ namespace System.CommandLine.Parsing
         public TokenType Type { get; }
 
         /// <inheritdoc />
-        public override bool Equals(object obj) =>
-            obj is Token token &&
-            Value == token.Value &&
-            Type == token.Type;
+        public override bool Equals(object obj) => obj is Token other && Equals(other);
 
         /// <inheritdoc />
-        public override int GetHashCode() => (Value, Type).GetHashCode();
+        public bool Equals(Token other) => Value == other.Value && Type == other.Type;
+
+        /// <inheritdoc />
+        public override int GetHashCode() => Value.GetHashCode() ^ (int)Type;
 
         /// <inheritdoc />
         public override string ToString() => $"{Type}: {Value}";
@@ -68,10 +66,7 @@ namespace System.CommandLine.Parsing
         /// <param name="left">The first <see cref="Token"/>.</param>
         /// <param name="right">The second <see cref="Token"/>.</param>
         /// <returns><see langword="true" /> if the objects are equal.</returns>
-        public static bool operator ==(Token left, Token right)
-        {
-            return EqualityComparer<Token>.Default.Equals(left, right);
-        }
+        public static bool operator ==(Token left, Token right) => left.Equals(right);
 
         /// <summary>
         /// Checks if two specified <see cref="Token"/> instances have different values.
@@ -79,10 +74,7 @@ namespace System.CommandLine.Parsing
         /// <param name="left">The first <see cref="Token"/>.</param>
         /// <param name="right">The second <see cref="Token"/>.</param>
         /// <returns><see langword="true" /> if the objects are not equal.</returns>
-        public static bool operator !=(Token left, Token right)
-        {
-            return !(left == right);
-        }
+        public static bool operator !=(Token left, Token right) => !left.Equals(right);
 
         internal bool IsFirstCharOfTheUnprefixedValue(char c)
         {
