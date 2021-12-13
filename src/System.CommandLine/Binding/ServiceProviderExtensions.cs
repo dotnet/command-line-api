bb70cc3b3a9d54198142548c951529e7744e088b
Jon Sequeira <jonsequeira@gmail.com>
2021-12-13 13:03:36 -0800

    proof of concept for SetHandler with injected services support

---
+++ src/System.CommandLine/Binding/ServiceProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.CommandLine.Binding;
+
+internal static class ServiceProviderExtensions
+{
+    public static T GetService<T>(this IServiceProvider serviceProvider)
+    {
+        var service = serviceProvider.GetService(typeof(T));
+
+        if (service is null)
+        {
+            throw new ArgumentException($"Service not found: {typeof(T)}");
+        }
+
+        return (T)service;
+    }
+}
--- src/System.CommandLine/Invocation/CommandHandler.SetHandler.cs
+++ src/System.CommandLine/Invocation/CommandHandler.SetHandler.cs
@@ -1,15 +1,44 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.CommandLine.Binding;
+
 namespace System.CommandLine.Invocation;
 
 public static partial class CommandHandler
 {
     public static ICommandHandler SetHandler<T1, T2, T3>(
         this Command command,
-        object symbol1,
-        object symbol2,
-        Action<T1, T2> handle) =>
+        Action<T1, T2, T3> handle,
+        params IValueDescriptor[] symbols) =>
         command.Handler = new AnonymousCommandHandler(
-            context => { });
+            context =>
+            {
+                var index = 0;
+
+                var value1 = GetParsedValueOrService<T1>(symbols, ref index, context);
+                var value2 = GetParsedValueOrService<T2>(symbols, ref index, context);
+                var value3 = GetParsedValueOrService<T3>(symbols, ref index, context);
+
+                handle(value1,
+                       value2,
+                       value3);
+            });
+
+    private static T? GetParsedValueOrService<T>(
+        IValueDescriptor[] symbols,
+        ref int index,
+        InvocationContext context)
+    {
+        if (symbols.Length > index &&
+            symbols[index] is IValueDescriptor<T> symbol1)
+        {
+            index++;
+            return context.ParseResult.GetValueFor(symbol1);
+        }
+        else
+        {
+            return context.BindingContext.GetService<T>();
+        }
+    }
 }
