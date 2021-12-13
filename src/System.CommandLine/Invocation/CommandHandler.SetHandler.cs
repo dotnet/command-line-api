b00af8b95fd16b740bab9a9aaee06806ee2d2a71
Jon Sequeira <jonsequeira@gmail.com>
2021-12-13 10:57:24 -0800

    wip

---
+++ src/System.CommandLine/Invocation/CommandHandler.SetHandler.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.CommandLine.Invocation;
+
+public static partial class CommandHandler
+{
+    public static ICommandHandler SetHandler<T1, T2, T3>(
+        this Command command,
+        object symbol1,
+        object symbol2,
+        Action<T1, T2> handle) =>
+        command.Handler = new AnonymousCommandHandler(
+            context => { });
+}
