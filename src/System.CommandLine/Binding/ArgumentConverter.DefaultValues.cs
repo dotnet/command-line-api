bd0b3c93017007e9736563379ac824b4033137a6
Adam Sitnik <adam.sitnik@gmail.com>
2022-02-25 15:21:22 +0100

    Add link to the issue

--- src/System.CommandLine/Binding/ArgumentConverter.DefaultValues.cs
+++ src/System.CommandLine/Binding/ArgumentConverter.DefaultValues.cs
@@ -19,7 +19,7 @@ internal static partial class ArgumentConverter
                   .SingleOrDefault(c => c.GetParameters().Length == 0)!);
 #endif
 
-    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL3050", Justification = "We are working on it")]
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL3050", Justification = "https://github.com/dotnet/command-line-api/issues/1638")]
     private static Array CreateArray(Type itemType, int capacity = 0)
         => Array.CreateInstance(itemType, capacity);
 
