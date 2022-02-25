8149729e362aac97c33025e5503bd99b63e0e3f4
Adam Sitnik <adam.sitnik@gmail.com>
2022-02-25 13:40:48 +0100

    add NativeAOT test

---
+++ src/System.CommandLine.Tests/NativeAOTTests.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if NET6_0_OR_GREATER
+
+using System.CommandLine.Suggest;
+using System.CommandLine.Tests.Utility;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System.CommandLine.Tests;
+
+public class NativeAOTTests
+{
+    private readonly ITestOutputHelper _output;
+    private readonly string _systemCommandLineDllPath;
+
+    public NativeAOTTests(ITestOutputHelper output)
+    {
+        _output = output;
+
+        _systemCommandLineDllPath = typeof(Command).Assembly.Location;
+    }
+
+    [ReleaseBuildOnlyTheory]
+    [InlineData("-p:IlcOptimizationPreference=Speed")]
+    [InlineData("-p:IlcOptimizationPreference=Size")]
+    public void App_referencing_system_commandline_can_be_compiled_ahead_of_time(string additionalArgs)
+    {
+        var stdOut = new StringBuilder();
+        var stdErr = new StringBuilder();
+
+        var workingDirectory = Path.Combine(Directory.GetCurrentDirectory(), "TestApps", "NativeAOT");
+        
+        Process.RunToCompletion(
+            DotnetMuxer.Path.FullName,
+            "clean -c Release -r win-x64",
+            workingDirectory: workingDirectory);
+
+        var commandLine = string.Format(
+            "publish -c Release -r win-x64 --self-contained -p:SystemCommandLineDllPath=\"{0}\" -p:TreatWarningsAsErrors=true {1}",
+            _systemCommandLineDllPath,
+            additionalArgs);
+
+        var exitCode = Process.RunToCompletion(
+            DotnetMuxer.Path.FullName,
+            commandLine,
+            s =>
+            {
+                _output.WriteLine(s);
+                stdOut.Append(s);
+            },
+            s =>
+            {
+                _output.WriteLine(s);
+                stdErr.Append(s);
+            },
+            workingDirectory);
+
+        stdOut.ToString().Should().NotContain("warning IL");
+        stdErr.ToString().Should().BeEmpty();
+        exitCode.Should().Be(0);
+    }
+}
+
+#endif
---
+++ src/System.CommandLine.Tests/TestApps/NativeAOT/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+
+public class Program
+{
+    static void Run(bool boolean, string text)
+    {
+        Console.WriteLine($"Bool option: {text}");
+        Console.WriteLine($"String option: {boolean}");
+    }
+
+    private static int Main(string[] args)
+    {
+        Option<bool> boolOption = new Option<bool>(new[] { "--bool", "-b" }, "Bool option");
+        Option<string> stringOption = new Option<string>(new[] { "--string", "-s" }, "String option");
+
+        RootCommand command = new RootCommand
+        {
+            boolOption,
+            stringOption
+        };
+
+        command.SetHandler<bool, string>(Run, boolOption, stringOption);
+
+        return new CommandLineBuilder(command).Build().Invoke(args);
+    }
+}
