3ed2081d08a1fea11004a77f4685320eada8197e
Jan Krivanek <jankrivanek@microsoft.com>
2022-06-07 22:05:51 +0200

    Fix tests - do not rely on captured closure

--- src/System.CommandLine.Tests/Invocation/CancelOnProcessTerminationTests.cs
+++ src/System.CommandLine.Tests/Invocation/CancelOnProcessTerminationTests.cs
@@ -93,10 +93,8 @@ namespace System.CommandLine.Tests.Invocation
 
         [LinuxOnlyTheory]
         [InlineData(null, SIGINT)]
-        [InlineData(100, SIGINT, Skip = "Doesn't work under RemoteExecutor - process not terminated after Environment.Exit")]
         [InlineData(null, SIGTERM)]
-        [InlineData(100, SIGTERM, Skip = "Doesn't work under RemoteExecutor - process not terminated after ExitProcess event")]
-        public async Task CancelOnProcessTermination_timeout_on_cancel_processing(int? timeOutMs, int signo)
+        public async Task CancelOnProcessTermination_null_timeout_on_cancel_processing(int? timeOutMs, int signo)
         {
             TimeSpan? timeOut = timeOutMs.HasValue ? TimeSpan.FromMilliseconds(timeOutMs.Value) : null; 
 
@@ -124,22 +122,110 @@ namespace System.CommandLine.Tests.Invocation
                         // command is executed.
                         // We are currently blocking that event because CancellationTokenSource.Cancel
                         // is called from the event handler.
-                        // We'll do an async Task.Delay now. This means the Cancel call will return
+                        // We'll do an async Yield now. This means the Cancel call will return
                         // and we're no longer actively blocking the event.
                         // The event handler is responsible to continue blocking until the command
                         // has finished executing. If it doesn't we won't get the CancelledExitCode.
-                        await Task.Delay(TimeSpan.FromMilliseconds(2000));
+                        await Task.Yield();
 
-                        context.ExitCode = CancelledExitCode;
+                        // Exit code gets set here - but then execution continues and is let run till code voluntarily returns
+                        //  hence exit code gets overwritten below
+                        context.ExitCode = 123;
                     }
 
+                    // This is an example of bad pattern and reason why we need a timeout on termination processing
+                    await Task.Delay(TimeSpan.FromMilliseconds(200));
+
+                    context.ExitCode = CancelledExitCode;
                 });
 
                 return new CommandLineBuilder(new RootCommand
                        {
                            command
                        })
-                       .CancelOnProcessTermination(/*timeOut*/TimeSpan.FromSeconds(100))
+                       // Unfortunately we cannot use test parameter here - RemoteExecutor currently doesn't capture the closure
+                       .CancelOnProcessTermination(null)
+                       .Build()
+                       .InvokeAsync("the-command");
+            };
+
+            using RemoteExecution program = RemoteExecutor.Execute(childProgram, psi: new ProcessStartInfo { RedirectStandardOutput = true });
+
+            Process process = program.Process;
+
+            // Wait for the child to be in the command handler.
+            string childState = await process.StandardOutput.ReadLineAsync();
+            childState.Should().Be(ChildProcessWaiting);
+
+            // Request termination
+            kill(process.Id, signo).Should().Be(0);
+
+            // Verify the process terminates timely
+            bool processExited = process.WaitForExit(10000);
+            if (!processExited)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+            processExited.Should().Be(true);
+
+            // Verify the process exit code
+            process.ExitCode.Should().Be(timeOutMs.HasValue ? ForceTerminationCode : CancelledExitCode);
+        }
+
+        [LinuxOnlyTheory]
+        [InlineData(100, SIGINT)]
+        [InlineData(100, SIGTERM)]
+        public async Task CancelOnProcessTermination_timeout_on_cancel_processing(int? timeOutMs, int signo)
+        {
+            TimeSpan? timeOut = timeOutMs.HasValue ? TimeSpan.FromMilliseconds(timeOutMs.Value) : null;
+
+            const string ChildProcessWaiting = "Waiting for the command to be cancelled";
+            const int CancelledExitCode = 42;
+            const int ForceTerminationCode = 130;
+
+            Func<string[], Task<int>> childProgram = (string[] args) =>
+            {
+                var command = new Command("the-command");
+
+                command.SetHandler(async context =>
+                {
+                    var cancellationToken = context.GetCancellationToken();
+
+                    try
+                    {
+                        context.Console.WriteLine(ChildProcessWaiting);
+                        await Task.Delay(int.MaxValue, cancellationToken);
+                        context.ExitCode = 1;
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // For Process.Exit handling the event must remain blocked as long as the
+                        // command is executed.
+                        // We are currently blocking that event because CancellationTokenSource.Cancel
+                        // is called from the event handler.
+                        // We'll do an async Yield now. This means the Cancel call will return
+                        // and we're no longer actively blocking the event.
+                        // The event handler is responsible to continue blocking until the command
+                        // has finished executing. If it doesn't we won't get the CancelledExitCode.
+                        await Task.Yield();
+
+                        context.ExitCode = CancelledExitCode;
+                    }
+
+                    // This is an example of bad pattern and reason why we need a timeout on termination processing
+                    await Task.Delay(TimeSpan.FromMilliseconds(2000));
+
+                    // Execution should newer get here as termination processing has a timeout of 100ms
+                    context.ExitCode = 123;
+                });
+
+                return new CommandLineBuilder(new RootCommand
+                       {
+                           command
+                       })
+                        // Unfortunately we cannot use test parameter here - RemoteExecutor currently doesn't capture the closure
+                       .CancelOnProcessTermination(TimeSpan.FromMilliseconds(100))
                        .Build()
                        .InvokeAsync("the-command");
             };
