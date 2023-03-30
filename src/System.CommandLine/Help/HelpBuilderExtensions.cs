6706d6556d87da814ae0dfbfe5b2088f8d8e5617
Adam Sitnik <adam.sitnik@gmail.com>
2023-03-30 17:16:22 +0200

    merge HelpBuilderExtensions into HelpBuilder (#2130)
    * address code review feedback
    
    * merge HelpBuilderExtensions into HelpBuilder

--- src/System.CommandLine/Help/HelpBuilderExtensions.cs
+++ src/System.CommandLine/Help/HelpBuilderExtensions.cs
@@ -5,38 +5,30 @@ using System.IO;
 
 namespace System.CommandLine.Help
 {
-    /// <summary>
-    /// Provides extension methods for the help builder.
-    /// </summary>
-    public static class HelpBuilderExtensions
+    public partial class HelpBuilder
     {
         /// <summary>
         /// Specifies custom help details for a specific symbol.
         /// </summary>
-        /// <param name="builder">The help builder to write with.</param>
         /// <param name="symbol">The symbol to customize the help details for.</param>
         /// <param name="firstColumnText">A delegate to display the first help column (typically name and usage information).</param>
         /// <param name="secondColumnText">A delegate to display second help column (typically the description).</param>
         /// <param name="defaultValue">The displayed default value for the symbol.</param>
-        public static void CustomizeSymbol(
-            this HelpBuilder builder,
+        public void CustomizeSymbol(
             Symbol symbol,
             string? firstColumnText = null,
             string? secondColumnText = null,
             string? defaultValue = null)
         {
-            builder.CustomizeSymbol(symbol, _ => firstColumnText, _ => secondColumnText, _ => defaultValue);
+            CustomizeSymbol(symbol, _ => firstColumnText, _ => secondColumnText, _ => defaultValue);
         }
 
         /// <summary>
         /// Writes help output for the specified command.
         /// </summary>
-        public static void Write(
-            this HelpBuilder helpBuilder,
-            Command command,
-            TextWriter writer)
+        public void Write(Command command, TextWriter writer)
         {
-            helpBuilder.Write(new HelpContext(helpBuilder, command, writer));
+            Write(new HelpContext(this, command, writer));
         }
     }
 }
