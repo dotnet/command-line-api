cf33856bed1ce663861fe0f97d5e073d1ba485c7
Adam Sitnik <adam.sitnik@gmail.com>
2023-06-06 16:47:10 +0200

    add a failing test

--- src/System.CommandLine.Tests/UseParseErrorReportingTests.cs
+++ src/System.CommandLine.Tests/UseParseErrorReportingTests.cs
@@ -60,5 +60,46 @@ namespace System.CommandLine.Tests
 
             result.Should().Be(42);
         }
+
+        [Fact]
+        public void User_can_customize_help_printed_on_parse_error()
+        {
+            CustomHelpBuilder customHelpBuilder = new();
+
+            CliRootCommand root = new ();
+            root.Options.Clear();
+            root.Options.Add(new HelpOption()
+            {
+                Action = new HelpAction()
+                {
+                    Builder = customHelpBuilder
+                }
+            });
+
+            CliConfiguration config = new(root)
+            {
+                EnableParseErrorReporting = true
+            };
+
+            customHelpBuilder.WasUsed.Should().BeFalse();
+
+            ParseResult parseResult = root.Parse("-bla", config);
+
+            int result = parseResult.Invoke();
+            result.Should().Be(1);
+            customHelpBuilder.WasUsed.Should().BeTrue();
+        }
+
+        private sealed class CustomHelpBuilder : HelpBuilder
+        {
+            internal bool WasUsed = false;
+
+            public override void Write(HelpContext context)
+            {
+                WasUsed = true;
+
+                base.Write(context);
+            }
+        }
     }
 }
