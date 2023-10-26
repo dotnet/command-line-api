7a87dc96e1045a6a2799ebe84678498ee4307afe
Your Name <you@example.com>
2023-10-26 20:46:09 +0100

    2279 - remove unused declarations

--- src/System.CommandLine.Tests/CompletionContextTests.cs
+++ src/System.CommandLine.Tests/CompletionContextTests.cs
@@ -2,10 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.CommandLine.Completions;
-using System.Linq;
-using System.Runtime.InteropServices;
 using FluentAssertions;
-using FluentAssertions.Specialized;
 using Xunit;
 
 namespace System.CommandLine.Tests
