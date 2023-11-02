33d8733779a734738164eda2c134c7a5e8c93840
Viktor Hofer <viktor.hofer@microsoft.com>
2023-11-02 10:51:55 +0100

    Update CompilationTests.cs

--- src/System.CommandLine.Tests/CompilationTests.cs
+++ src/System.CommandLine.Tests/CompilationTests.cs
@@ -6,6 +6,7 @@
 using System.CommandLine.Suggest;
 using System.CommandLine.Tests.Utility;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using FluentAssertions;
 using Microsoft.DotNet.PlatformAbstractions;
