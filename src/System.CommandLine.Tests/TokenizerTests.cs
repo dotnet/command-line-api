851446aeaa0ee167bf51b9f9d7fa28dfc8243922
KathleenDollard <kdollard@microsoft.com>
2024-02-18 10:57:11 -0500

    Added tokenizer test

---
+++ src/System.CommandLine.Tests/TokenizerTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.CommandLine.Parsing;
+using System.IO;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
+using System.Linq;
+using FluentAssertions.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace System.CommandLine.Tests
+{
+    public partial class TokenizerTests
+    {
+
+        [Fact]
+        public void The_tokenizer_is_accessible()
+        {
+            var option = new CliOption<string>("--hello");
+            var command = new CliRootCommand { option };
+            IReadOnlyList<string> args = ["--hello", "world"];
+            List<CliToken> tokens = null;
+            List<string> errors = null;
+            CliTokenizer.Tokenize(args,command,false, true, out tokens, out errors);
+
+            //result.GetResult(animalsOption)
+            //      .Tokens
+            //      .Select(t => t.Value)
+            //      .Should()
+            //      .BeEquivalentTo("cat", "dog");
+
+            tokens
+                .Skip(1)
+                .Select(t => t.Value)
+                .Should()
+                .BeEquivalentTo("--hello", "world");
+
+            errors.Should().BeNull();
+        }
+    }
+}
