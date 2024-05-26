f0ead71eba1c5f2243a27e31522fc5fa6965104f
KathleenDollard <kdollard@microsoft.com>
2024-05-26 09:38:38 -0400

    Added new subsystems

---
+++ src/System.CommandLine.Subsystems/Subsystems/Annotations/InvocationAnnotations.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.CommandLine.Subsystems.Annotations;
+
+/// <summary>
+/// IDs for well-known Version annotations.
+/// </summary>
+public static class InvocationAnnotations
+{
+    internal static string Prefix { get; } = nameof(SubsystemKind.Invocation);
+
+}
