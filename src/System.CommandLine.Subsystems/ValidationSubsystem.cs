0350d6c3a3cf887bc5b1cc080593d7a6af9af67b
Kathleen Dollard <kdollard@microsoft.com>
2024-07-28 10:47:41 -0400

    First draft of validation

--- src/System.CommandLine.Subsystems/ValidationSubsystem.cs
+++ src/System.CommandLine.Subsystems/ValidationSubsystem.cs
@@ -1,52 +1,89 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.CommandLine.Parsing;
 using System.CommandLine.Subsystems;
 using System.CommandLine.Subsystems.Annotations;
 using System.CommandLine.Validation;
+using System.CommandLine.Validation.DataTraits;
 
 namespace System.CommandLine;
 
 public class ValidationSubsystem(IAnnotationProvider? annotationProvider = null)
     : CliSubsystem(ValidationAnnotations.Prefix, SubsystemKind.Validation, annotationProvider)
 {
-    private readonly Dictionary<Type, List<Validator>> validators = [];
+    // The Type key is the DataTrait type
+    private readonly Dictionary<Type, Validator> validators = [];
 
-    public void AddValidator(Type type, Validator validator)
-    {
-        if (!validators.TryGetValue(type, out var validatorList))
-        {
-            validators.Add(type, [validator]);
-            return;
-        }
-        validatorList.Add(validator);
-    }
+    public void AddValidator<TDataTrait>(Validator validator)
+        where TDataTrait : DataTrait
+        => validators[typeof(TDataTrait)] = validator;
 
-    public void RemoveValidator(Type type, Validator validator)
-    {
-        if (!validators.TryGetValue(type, out var validatorList))
-        {
-            return; // nothing to do
-        }
-        validatorList.Remove(validator);
-    }
+    public void RemoveValidator<TDataTrait>(Validator validator)
+        where TDataTrait : DataTrait
+        => validators.Remove(typeof(TDataTrait));
 
-    public void ClearValidatorsForType(Type type)
-    {
-        if (!validators.TryGetValue(type, out var validatorList))
-        {
-            return; // nothing to do
-        }
-        validatorList.Clear();
-    }
-
-    public void ClearAllValidators(Type type)
-    {
-        validators.Clear();
-    }
+    public void ClearValidators(Type type)
+        => validators.Clear();
 
     protected internal override void Execute(PipelineResult pipelineResult)
     {
-       // var symbolResults = pipelineResult.ParseResult.SymbolResults();
+        var errors = new List<ParseError>();
+        if (pipelineResult.ParseResult is null)
+        {
+            return; // nothing to do
+        }
+        // Not sure whether to do commands or values first
+        ValidateCommands(pipelineResult.ParseResult.CommandValueResult, errors, validators, pipelineResult.Pipeline, this);
+        ValidateValues(pipelineResult.ParseResult.AllValueResults, errors, validators, pipelineResult.Pipeline, this);
+
+        static void ValidateValues(IEnumerable<ValueResult> allValueResults, List<ParseError> errors,
+            Dictionary<Type, Validator> validators, Pipeline pipeline, ValidationSubsystem validationSubsystem)
+        {
+            foreach (var result in allValueResults)
+            {
+                if (result.ValueSymbol is not CliDataSymbol dataSymbol)
+                {
+                    return; // nothing to do
+                }
+                GetValidationErrors(errors, validators, pipeline, validationSubsystem, dataSymbol);
+            }
+        }
+
+        static void ValidateCommands(CommandValueResult commandValueResult, List<ParseError> errors,
+            Dictionary<Type, Validator> validators, Pipeline pipeline, ValidationSubsystem validationSubsystem)
+        {
+            // Walk up the results tree. Not needed for ValueResults because they are collapsed
+            var current = commandValueResult;
+            while (current is not null)
+            {
+                var symbol = current.Command;
+                GetValidationErrors(errors, validators, pipeline, validationSubsystem, symbol);
+                current = current.Parent;
+            }
+        }
+
+        static void GetValidationErrors(List<ParseError> errors, Dictionary<Type, Validator> validators,
+            Pipeline pipeline, ValidationSubsystem validationSubsystem, CliSymbol symbol)
+        {
+            var traits = symbol.GetDataTraits();
+            if (traits is null)
+            {
+                return;
+            }
+            foreach (var trait in traits)
+            {
+                if (!validators.TryGetValue(trait.GetType(), out var validator))
+                {
+                    // TODO: This seems an issue - an exception or an error that a validator is missing
+                    continue;
+                }
+                var newErrors = validator.Validate(symbol, trait, pipeline, validationSubsystem);
+                if (newErrors is not null)
+                {
+                    errors.AddRange(newErrors);
+                }
+            }
+        }
     }
 }
