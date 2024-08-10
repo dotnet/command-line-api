83855a57d9d4e8facd397521793b7ce53e3237b6
Kathleen Dollard <kdollard@microsoft.com>
2024-08-10 11:51:57 -0400

    Add Error tracking to PipelineResult

--- src/System.CommandLine.Subsystems/PipelineResult.cs
+++ src/System.CommandLine.Subsystems/PipelineResult.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.CommandLine.Parsing;
+
 namespace System.CommandLine;
 
 public class PipelineResult(ParseResult? parseResult, string rawInput, Pipeline? pipeline, ConsoleHack? consoleHack = null)
 {
+    private readonly List<ParseError> errors = [];
     public ParseResult? ParseResult { get; } = parseResult;
     public string RawInput { get; } = rawInput;
     public Pipeline Pipeline { get; } = pipeline ?? Pipeline.CreateEmpty();
@@ -12,6 +15,22 @@ public class PipelineResult(ParseResult? parseResult, string rawInput, Pipeline?
 
     public bool AlreadyHandled { get; set; }
     public int ExitCode { get; set; }
+
+    public void AddErrors(IEnumerable<ParseError> errors)
+    {
+        if (errors is not null)
+        {
+            this.errors.AddRange(errors);
+        }
+    }
+
+    public void AddError(ParseError error)
+        => errors.Add(error);
+
+    public IEnumerable<ParseError> GetErrors(bool excludeParseErrors = false)
+        => excludeParseErrors || ParseResult is null
+            ? errors
+            : ParseResult.Errors.Concat(errors);
 
     public void NotRun(ParseResult? parseResult)
     {
