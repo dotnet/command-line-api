e00a84929dd72d74316317558001d56f4f6e1319
poonambhattad <157356390+poonambhattad@users.noreply.github.com>
2024-09-19 21:49:04 +0530

    String casing validations added
    Lower and upper case command options validations added.

---
+++ StringCase.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine.Parsing;
+using System.CommandLine.Validation;
+using System.CommandLine.ValueSources;
+using System.Globalization;
+
+namespace System.CommandLine.ValueConditions;
+
+///// <summary>
+///// Declares the string casing condition for the option or argument.
+///// </summary>
+///// <remarks>
+///// The non-generic version is used by the <see cref="StringValidator/>.
+///// </remarks>
+//public abstract class StringCase : ValueCondition
+//{
+//    protected StringCase(string valueType)
+//        : base(nameof(StringCase))
+//    {
+//        ValueType = valueType;
+//    }
+
+//    /// <summary>
+//    /// The type of the symbol the casing applies to.
+//    /// </summary>
+//    public string ValueType { get; }
+//}
+
+/// <summary>
+/// Declares the casing condition for the option or argument. Instances 
+/// of this method are created via extension methods on <see cref="ValueSymbol"/>
+/// </summary>
+/// <typeparam name="T">The type of the symbol the range applies to.</typeparam>
+public class StringCase : ValueCondition, IValueValidator
+{
+    internal StringCase(ValueSource<string>? casing) : base(nameof(StringCase))
+    {
+        Casing = casing;
+    }
+
+    /// <inheritdoc/>
+    public void Validate(object? value,
+                         CliValueSymbol valueSymbol,
+                         CliValueResult? valueResult,
+                         ValueCondition valueCondition,
+                         ValidationContext validationContext)
+    {
+        if (valueCondition != this) throw new InvalidOperationException("Unexpected value condition type");
+        if (value is not string stringValue) throw new InvalidOperationException("Unexpected value type");
+
+        if (stringValue is null) return; // nothing to do
+
+        // TODO: Replace the strings we are comparing with a diagnostic ID when we update ParseError
+        if (Casing is not null
+            && validationContext.TryGetTypedValue(Casing, out var casingValue))
+        {
+            if (casingValue is null) return;
+            if (casingValue == "lower"
+                && !stringValue.Equals(stringValue.ToLower(CultureInfo.CurrentCulture)))
+            {
+                validationContext.AddError(new ParseError($"The value for '{valueSymbol.Name}' is not in lower case."));
+            }
+            if (casingValue == "upper"
+                && !stringValue.Equals(stringValue.ToUpper(CultureInfo.CurrentCulture)))
+            {
+                validationContext.AddError(new ParseError($"The value for '{valueSymbol.Name}' is not in upper case."));
+            }
+        }
+    }
+
+    /// <summary>
+    /// The expected casing.
+    /// </summary>
+    public ValueSource<string>? Casing { get; init; }
+}
---
+++ StringValidationTests.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using FluentAssertions;
+using System.CommandLine.Parsing;
+using System.CommandLine.ValueSources;
+using Xunit;
+
+namespace System.CommandLine.Subsystems.Tests;
+
+public class StringValidationTests
+{
+    private PipelineResult ExecutedPipelineResultForCommand(CliCommand command, string input)
+    {
+        var validationSubsystem = ValidationSubsystem.Create();
+        var parseResult = CliParser.Parse(command, input, new CliConfiguration(command));
+        var pipelineResult = new PipelineResult(parseResult, input, Pipeline.CreateEmpty());
+        validationSubsystem.Execute(pipelineResult);
+        return pipelineResult;
+    }
+
+    [Fact]
+    public void StringValidationShouldPassWhenInputInLowerCase()
+    {
+        var option = new CliOption<string>("--opt");
+        option.SetCasing("lower");
+        var command = new CliCommand("cmd");
+        command.Options.Add(option);
+
+        var input = "--opt letuswin";
+
+        var pipelineResult = ExecutedPipelineResultForCommand(command, input);
+
+        pipelineResult.Should().NotBeNull();
+        pipelineResult.GetErrors().Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StringValidationShouldFailWhenInputNotInLowerCase()
+    {
+        var option = new CliOption<string>("--opt");
+        option.SetCasing("lower");
+        var command = new CliCommand("cmd");
+        command.Options.Add(option);
+
+        var input = "--opt LetUsWin";
+
+        var pipelineResult = ExecutedPipelineResultForCommand(command, input);
+
+        pipelineResult.Should().NotBeNull();
+        pipelineResult.GetErrors().Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void StringValidationShouldFailWhenInputNotInUpperCase()
+    {
+        var option = new CliOption<string>("--opt");
+        option.SetCasing("lower");
+        var command = new CliCommand("cmd");
+        command.Options.Add(option);
+
+        var input = "--opt LetUsWin";
+
+        var pipelineResult = ExecutedPipelineResultForCommand(command, input);
+
+        pipelineResult.Should().NotBeNull();
+        pipelineResult.GetErrors().Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void StringValidationShouldPassWhenInputInUpperCase()
+    {
+        var option = new CliOption<string>("--opt");
+        option.SetCasing("upper");
+        var command = new CliCommand("cmd");
+        command.Options.Add(option);
+
+        var input = "--opt GOODWORK!";
+
+        var pipelineResult = ExecutedPipelineResultForCommand(command, input);
+
+        pipelineResult.Should().NotBeNull();
+        pipelineResult.GetErrors().Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StringValidationShouldThrowExceptionWhenInputIsEmptyAndCasingIsSet()
+    {
+        var option = new CliOption<string>("--opt");
+        option.SetCasing("lower");
+        var command = new CliCommand("cmd");
+        command.Options.Add(option);
+
+        var input = "--opt ";
+
+        Assert.Throws<InvalidOperationException>(() => ExecutedPipelineResultForCommand(command, input));
+    }
+
+    [Fact]
+    public void StringValidationShouldPassWhenInputIsAllNumericWithLower()
+    {
+        var option = new CliOption<string>("--opt");
+        option.SetCasing("lower");
+        var command = new CliCommand("cmd");
+        command.Options.Add(option);
+
+        var input = "--opt fuzzy123";
+
+        var pipelineResult = ExecutedPipelineResultForCommand(command, input);
+
+        pipelineResult.Should().NotBeNull();
+        pipelineResult.GetErrors().Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StringValidationShouldPassWhenInputIsAllNumericWithUpper()
+    {
+        var option = new CliOption<string>("--opt");
+        option.SetCasing("upper");
+        var command = new CliCommand("cmd");
+        command.Options.Add(option);
+
+        var input = "--opt FUZZY123";
+
+        var pipelineResult = ExecutedPipelineResultForCommand(command, input);
+
+        pipelineResult.Should().NotBeNull();
+        pipelineResult.GetErrors().Should().BeEmpty();
+    }
+}
