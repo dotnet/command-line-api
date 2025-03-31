6a562ce00479ab1d95141745dc613cefc4f44b74
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2025-03-31 23:01:17 +0200

    Delete HostingAction and remove BindingHandler dependency and extension methods

--- src/System.CommandLine.Hosting/HostingExtensions.cs
+++ src/System.CommandLine.Hosting/HostingExtensions.cs
@@ -1,60 +1,11 @@
-﻿using System.CommandLine.Binding;
-using System.CommandLine.Invocation;
-using System.CommandLine.NamingConventionBinder;
-using CommandHandler = System.CommandLine.NamingConventionBinder.CommandHandler;
+﻿#nullable enable
 
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 
 namespace System.CommandLine.Hosting
 {
     public static class HostingExtensions
     {
-        public static CommandLineConfiguration UseHost(
-            this CommandLineConfiguration config,
-            Func<string[], IHostBuilder> hostBuilderFactory,
-            Action<IHostBuilder> configureHost = null)
-        {
-            if (config.RootCommand is RootCommand root)
-            {
-                root.Add(new Directive(HostingAction.HostingDirectiveName));
-            }
-
-            HostingAction.SetHandlers(config.RootCommand, hostBuilderFactory, configureHost);
-
-            return config;
-        }
-
-        public static CommandLineConfiguration UseHost(
-            this CommandLineConfiguration config,
-            Action<IHostBuilder> configureHost = null
-        ) => UseHost(config, null, configureHost);
-
-        public static OptionsBuilder<TOptions> BindCommandLine<TOptions>(
-            this OptionsBuilder<TOptions> optionsBuilder)
-            where TOptions : class
-        {
-            if (optionsBuilder is null)
-                throw new ArgumentNullException(nameof(optionsBuilder));
-            return optionsBuilder.Configure<IServiceProvider>((opts, serviceProvider) =>
-            {
-                var modelBinder = serviceProvider
-                    .GetService<ModelBinder<TOptions>>()
-                    ?? new ModelBinder<TOptions>();
-                var bindingContext = serviceProvider.GetRequiredService<BindingContext>();
-                modelBinder.UpdateInstance(opts, bindingContext);
-            });
-        }
-
-        public static Command UseCommandHandler<THandler>(this Command command)
-            where THandler : CommandLineAction
-        {
-            command.Action = CommandHandler.Create(typeof(THandler).GetMethod(nameof(AsynchronousCommandLineAction.InvokeAsync)));
-
-            return command;
-        }
-
         public static ParseResult GetParseResult(this IHostBuilder hostBuilder)
         {
             _ = hostBuilder ?? throw new ArgumentNullException(nameof(hostBuilder));
@@ -75,13 +26,6 @@ namespace System.CommandLine.Hosting
                 return invocationContext;
 
             throw new InvalidOperationException("Host builder context has no command-line parse result registered to it.");
-        }
-
-        public static IHost GetHost(this ParseResult parseResult)
-        {
-            _ = parseResult ?? throw new ArgumentNullException(paramName: nameof(parseResult));
-            var hostModelBinder = new ModelBinder<IHost>();
-            return (IHost)hostModelBinder.CreateInstance(parseResult.GetBindingContext());
         }
     }
 }
