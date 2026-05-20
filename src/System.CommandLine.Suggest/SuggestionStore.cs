73057912e3421134ece3ed36ca44a89b95490aa1
Ahmed <ahmedhamouda18hhh@gmail.com>
2026-05-20 18:41:08 +0300

    Fix exit code handling using WaitForExit and remove duplicate assignment

--- src/System.CommandLine.Suggest/SuggestionStore.cs
+++ src/System.CommandLine.Suggest/SuggestionStore.cs
@@ -44,13 +44,13 @@ namespace System.CommandLine.Suggest
 
                     Task<string> readToEndTask = process.StandardOutput.ReadToEndAsync();
 
-                   if (readToEndTask.Wait(timeout))
+                   if (process.WaitForExit((int)timeout.TotalMilliseconds))
                     {
                         if(process.ExitCode == 0)
                         {
                             result = readToEndTask.Result;
                         }
-                        result = readToEndTask.Result;
+                        
                     }
                     else
                     {
